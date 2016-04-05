8fe774f92eed4179a163d4d9c5a0919852959711
Teijo Rönkä <teijo.ronka@solita.fi>
2016-04-05 15:02:56 +0300

    LD-20 added tests for rest-controllers

---
+++ src/test/java/fi/livi/digitraffic/tie/data/controller/FreeFlowSpeedRestTest.java
@@ -0,0 +1,29 @@
+package fi.livi.digitraffic.tie.data.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+
+import fi.livi.digitraffic.tie.RestTest;
+import fi.livi.digitraffic.tie.conf.MetadataApplicationConfiguration;
+
+public class FreeFlowSpeedRestTest extends RestTest {
+    @Test
+    public void testFreeFlowSpeedRestApi() throws Exception {
+        mockMvc.perform(get(MetadataApplicationConfiguration.API_V1_BASE_PATH + MetadataApplicationConfiguration.API_DATA_PART_PATH +
+                "/freeFlowSpeeds"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.localTime", Matchers.notNullValue())) //
+                .andExpect(jsonPath("$.utc", Matchers.notNullValue())) //
+                .andExpect(jsonPath("$.linkData", Matchers.notNullValue())) //
+                .andExpect(jsonPath("$.linkData[0].linkNo", Matchers.notNullValue()))
+                .andExpect(jsonPath("$.lamData", Matchers.notNullValue())) //
+                .andExpect(jsonPath("$.lamData[0].lamId", Matchers.notNullValue()));
+    }
+}
---
+++ src/test/java/fi/livi/digitraffic/tie/data/controller/LamDataRestTest.java
@@ -0,0 +1,27 @@
+package fi.livi.digitraffic.tie.data.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+
+import fi.livi.digitraffic.tie.RestTest;
+import fi.livi.digitraffic.tie.conf.MetadataApplicationConfiguration;
+
+public class LamDataRestTest extends RestTest {
+    @Test
+    public void testLamDataRestApi() throws Exception {
+        mockMvc.perform(get(MetadataApplicationConfiguration.API_V1_BASE_PATH + MetadataApplicationConfiguration.API_DATA_PART_PATH +
+                "/lamData"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.localTime", Matchers.notNullValue())) //
+                .andExpect(jsonPath("$.utc", Matchers.notNullValue())) //
+                .andExpect(jsonPath("$.dynamicLamData", Matchers.notNullValue())) //
+                .andExpect(jsonPath("$.dynamicLamData[0].lamId", Matchers.notNullValue()));
+    }
+}
