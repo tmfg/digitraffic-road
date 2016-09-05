14addbd820824f655c55ea639229e71442f5a729
Teijo Rönkä <teijo.ronka@solita.fi>
2016-09-05 10:55:40 +0300

    added cobertura-ignores

--- src/main/java/fi/livi/digitraffic/tie/metadata/controller/HomeController.java
+++ src/main/java/fi/livi/digitraffic/tie/metadata/controller/HomeController.java
@@ -1,7 +1,10 @@
 package fi.livi.digitraffic.tie.metadata.controller;
 
+import fi.livi.digitraffic.tie.annotation.CoverageIgnore;
+
 //@Controller
 //@Profile("localhost")
+@CoverageIgnore
 public class HomeController {
 //    @RequestMapping("/")
     public String home() {
