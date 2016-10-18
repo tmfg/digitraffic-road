a72620766349247dbd78f6bb287ba9ca5a163cfb
Jouni Sonninen <jouni.sonninen@solita.fi>
2016-10-18 10:07:22 +0300

    LD-23 Sanomavälitys SONJAn kautta - JMS -listener fix to be fault tolerant

--- src/main/java/fi/livi/digitraffic/tie/data/jms/JmsMessageListener.java
+++ src/main/java/fi/livi/digitraffic/tie/data/jms/JmsMessageListener.java
@@ -25,6 +25,9 @@ import fi.livi.digitraffic.tie.helper.ToStringHelpper;
 public abstract class JmsMessageListener<T> implements MessageListener {
 
     private static final Logger log = LoggerFactory.getLogger(JmsMessageListener.class);
+    private static final int MAX_NORMAL_QUEUE_SIZE = 100;
+    private static final int QUEUE_SIZE_WARNING_LIMIT = 2 * MAX_NORMAL_QUEUE_SIZE;
+    private static final int QUEUE_SIZE_ERROR_LIMIT = 10 * MAX_NORMAL_QUEUE_SIZE;
 
     private final JAXBContext jaxbContext;
     private final String name;
@@ -74,6 +77,11 @@ public abstract class JmsMessageListener<T> implements MessageListener {
             log.info(name + " received " + message.getClass().getSimpleName());
             T data = unmarshalMessage(message);
             blockingQueue.add(data);
+            if (blockingQueue.size() > QUEUE_SIZE_ERROR_LIMIT) {
+                log.error("JMS message queue size " + blockingQueue.size() + " exceeds error limit " + QUEUE_SIZE_ERROR_LIMIT);
+            } else if (blockingQueue.size() > QUEUE_SIZE_WARNING_LIMIT) {
+                log.warn("JMS message queue size " + blockingQueue.size() + " exceeds warning limit " + QUEUE_SIZE_WARNING_LIMIT);
+            }
         } else {
             log.error("Not handling any messages anymore because app is shutting down");
         }
