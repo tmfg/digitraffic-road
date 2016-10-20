37980f69897d3a4941d4f2784afc186fa4fe51a7
Ville Kähkönen <ville.kahkonen@solita.fi>
2016-10-20 12:50:45 +0300

    Revert "DPO-22 Keliennusteiden tiejaksot -metadatan hakeminen ja päivittäminen - First version of RoadSectionWeatherClient"
    This reverts commit 87509bb53300b086d4135392bc3f905a353037bd.

--- src/main/java/fi/livi/digitraffic/tie/metadata/service/roadconditions/RoadConditionsClient.java
+++ src/main/java/fi/livi/digitraffic/tie/metadata/service/roadconditions/RoadConditionsClient.java
@@ -1,13 +1,10 @@
 package fi.livi.digitraffic.tie.metadata.service.roadconditions;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import fi.livi.digitraffic.tie.metadata.service.forecastsection.ForecastSectionNaturalIdHelper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-import javax.annotation.PostConstruct;
-import java.text.SimpleDateFormat;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -21,16 +18,9 @@ public class RoadConditionsClient {
 
     private final String roadsUrl = "roads.php";
 
-    private final String roadConditionsUrl = "roadconditions.php";
-
     private RestTemplate restTemplate = new RestTemplate();
 
     private ObjectMapper objectMapper = new ObjectMapper();
-
-    @PostConstruct
-    private void postConstruct() {
-        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm"));
-    }
 
     public List<RoadSectionCoordinatesDto> getRoadSections() {
 
@@ -39,24 +29,9 @@ public class RoadConditionsClient {
         return response.entrySet().stream().map(this::mapRoadSection).collect(Collectors.toList());
     }
 
-    public List<RoadSectionWeatherDto> getRoadSectionWeather() {
-
-        LinkedHashMap<String, Object> response = restTemplate.getForObject(baseUrl + roadConditionsUrl, LinkedHashMap.class);
-
-        return response.entrySet().stream()
-                .filter(e -> ForecastSectionNaturalIdHelper.isNaturalId(e.getKey()))
-                .map(this::mapRoadSectionWeather).collect(Collectors.toList());
-    }
-
     private RoadSectionCoordinatesDto mapRoadSection(Map.Entry<String, Object> RoadSection) {
         RoadSectionCoordinatesEntry coordinatesEntry = objectMapper.convertValue(RoadSection.getValue(), RoadSectionCoordinatesEntry.class);
 
         return new RoadSectionCoordinatesDto(RoadSection.getKey(), coordinatesEntry.getName(), coordinatesEntry.getCoordinates());
-    }
-
-    private RoadSectionWeatherDto mapRoadSectionWeather(Map.Entry<String, Object> roadWeather) {
-        RoadSectionWeatherEntry roadSectionWeatherEntry = objectMapper.convertValue(roadWeather.getValue(), RoadSectionWeatherEntry.class);
-
-        return new RoadSectionWeatherDto(roadWeather.getKey(), roadSectionWeatherEntry.roadName, roadSectionWeatherEntry.weatherForecasts);
     }
 }
--- src/test/java/fi/livi/digitraffic/tie/metadata/service/forecastsection/ForecastSectionNaturalIdHelperTest.java
+++ src/test/java/fi/livi/digitraffic/tie/metadata/service/forecastsection/ForecastSectionNaturalIdHelperTest.java
@@ -3,8 +3,6 @@ package fi.livi.digitraffic.tie.metadata.service.forecastsection;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 public class ForecastSectionNaturalIdHelperTest {
 
@@ -23,21 +21,5 @@ public class ForecastSectionNaturalIdHelperTest {
     @Test
     public void getRoadSectionVersionNumberSucceeds() {
         assertEquals(10, ForecastSectionNaturalIdHelper.getRoadSectionVersionNumber(naturalId));
-    }
-
-    @Test
-    public void isNaturalIdSucceeds() {
-        assertTrue(ForecastSectionNaturalIdHelper.isNaturalId(naturalId));
-        assertTrue(ForecastSectionNaturalIdHelper.isNaturalId("03305_067_000_0"));
-        assertFalse(ForecastSectionNaturalIdHelper.isNaturalId("03305_067_000_01"));
-        assertFalse(ForecastSectionNaturalIdHelper.isNaturalId("03305_067_000"));
-        assertFalse(ForecastSectionNaturalIdHelper.isNaturalId("0305_067_000_0"));
-        assertFalse(ForecastSectionNaturalIdHelper.isNaturalId("00305_07_0000_0"));
-        assertFalse(ForecastSectionNaturalIdHelper.isNaturalId("00305_007_00_00"));
-        assertFalse(ForecastSectionNaturalIdHelper.isNaturalId("002305_07_000_0"));
-        assertFalse(ForecastSectionNaturalIdHelper.isNaturalId("002305_07_03300"));
-        assertFalse(ForecastSectionNaturalIdHelper.isNaturalId("002305507503300"));
-        assertFalse(ForecastSectionNaturalIdHelper.isNaturalId("03_05_067_000_0"));
-        assertFalse(ForecastSectionNaturalIdHelper.isNaturalId("03005_067_0_0_0"));
     }
 }
