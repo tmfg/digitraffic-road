82575a41affb38c7d8de7dc6d325cad86bb38db8
Ville Kähkönen <ville.kahkonen@solita.fi>
2016-12-02 10:58:17 +0200

    DPO-22 Forecast section data and metadata update jobs now extend SimpleUpdateJob

--- src/main/java/fi/livi/digitraffic/tie/metadata/quartz/ForecastSectionCoordinatesUpdateJob.java
+++ src/main/java/fi/livi/digitraffic/tie/metadata/quartz/ForecastSectionCoordinatesUpdateJob.java
@@ -1,16 +1,16 @@
 package fi.livi.digitraffic.tie.metadata.quartz;
 
-import fi.livi.digitraffic.tie.metadata.model.MetadataType;
-import fi.livi.digitraffic.tie.metadata.service.forecastsection.ForecastSectionMetadataUpdater;
 import org.quartz.DisallowConcurrentExecution;
 import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import fi.livi.digitraffic.tie.metadata.model.MetadataType;
+import fi.livi.digitraffic.tie.metadata.service.forecastsection.ForecastSectionMetadataUpdater;
+
 @DisallowConcurrentExecution
-public class ForecastSectionCoordinatesUpdateJob extends AbstractUpdateJob {
+public class ForecastSectionCoordinatesUpdateJob extends SimpleUpdateJob {
 
     private static final Logger log = LoggerFactory.getLogger(ForecastSectionCoordinatesUpdateJob.class);
 
@@ -18,19 +18,14 @@ public class ForecastSectionCoordinatesUpdateJob extends AbstractUpdateJob {
     private ForecastSectionMetadataUpdater forecastSectionMetadataUpdater;
 
     @Override
-    public void execute(JobExecutionContext context) throws JobExecutionException {
-        log.info("Road section coordinates update job start");
-        final long startTime = System.currentTimeMillis();
-
+    protected void doExecute(JobExecutionContext context) throws Exception {
         boolean updated = forecastSectionMetadataUpdater.updateForecastSectionMetadata();
 
         if (updated) {
             staticDataStatusService.updateMetadataUpdated(MetadataType.FORECAST_SECTION);
         }
 
-        final long endTime = System.currentTimeMillis();
-
-        String updateStatus = updated ? "Data updates took place." : "No updates took place.";
-        log.info("Road section coordinates update job ended. Update took " + (endTime - startTime) + " milliseconds. " + updateStatus);
+        String updateStatus = updated ? "Coordinates were updated." : "Coordinates were up-to-date.";
+        log.info(updateStatus);
     }
 }
--- src/main/java/fi/livi/digitraffic/tie/metadata/quartz/ForecastSectionWeatherUpdateJob.java
+++ src/main/java/fi/livi/digitraffic/tie/metadata/quartz/ForecastSectionWeatherUpdateJob.java
@@ -4,33 +4,21 @@ import java.time.ZonedDateTime;
 
 import org.quartz.DisallowConcurrentExecution;
 import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import fi.livi.digitraffic.tie.metadata.model.MetadataType;
 import fi.livi.digitraffic.tie.metadata.service.forecastsection.ForecastSectionDataUpdater;
 
 @DisallowConcurrentExecution
-public class ForecastSectionWeatherUpdateJob extends AbstractUpdateJob {
-
-    private static final Logger log = LoggerFactory.getLogger(ForecastSectionWeatherUpdateJob.class);
+public class ForecastSectionWeatherUpdateJob extends SimpleUpdateJob {
 
     @Autowired
     private ForecastSectionDataUpdater forecastSectionDataUpdater;
 
     @Override
-    public void execute(JobExecutionContext context) throws JobExecutionException {
-
-        log.info("Starting forecast section weather data update job.");
-        long start = System.currentTimeMillis();
-
+    protected void doExecute(JobExecutionContext context) throws Exception {
         ZonedDateTime messageTimestamp = forecastSectionDataUpdater.updateForecastSectionWeatherData();
 
         staticDataStatusService.setMetadataUpdated(MetadataType.FORECAST_SECTION_WEATHER, messageTimestamp);
-
-        long end = System.currentTimeMillis();
-        log.info("Ending forecast section weather data update job. Update took " + (end - start) + " milliseconds.");
     }
 }
