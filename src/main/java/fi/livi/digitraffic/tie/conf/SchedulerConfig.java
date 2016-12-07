c9dd5fe2ec98f55cd16305f4e1c6e07faa9f0614
Jouni Sonninen <jouni.sonninen@solita.fi>
2016-12-07 14:15:21 +0200

    Fixed unhandled datex messages schedule

--- src/main/java/fi/livi/digitraffic/tie/conf/SchedulerConfig.java
+++ src/main/java/fi/livi/digitraffic/tie/conf/SchedulerConfig.java
@@ -14,6 +14,7 @@ import org.quartz.Trigger;
 import org.quartz.spi.JobFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.PropertiesFactoryBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.ApplicationContext;
@@ -25,7 +26,12 @@ import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 import org.springframework.scheduling.quartz.SimpleTriggerFactoryBean;
 
 import fi.livi.digitraffic.tie.metadata.quartz.AutowiringSpringBeanJobFactory;
+import fi.livi.digitraffic.tie.metadata.quartz.CameraUpdateJob;
+import fi.livi.digitraffic.tie.metadata.quartz.LocationMetadataUpdateJob;
+import fi.livi.digitraffic.tie.metadata.quartz.RoadStationsStatusUpdateJob;
+import fi.livi.digitraffic.tie.metadata.quartz.TmsStationUpdateJob;
 import fi.livi.digitraffic.tie.metadata.quartz.UnhandledDatex2MessagesImportJob;
+import fi.livi.digitraffic.tie.metadata.quartz.WeatherStationUpdateJob;
 
 @Configuration
 @ConditionalOnProperty(name = "quartz.enabled")
@@ -127,8 +133,9 @@ public class SchedulerConfig {
     }
 
     @Bean
-    public SimpleTriggerFactoryBean unhandledDatex2MessagesImportJobTrigger(final JobDetail unhandledDatex2MessagesImportJobDetail) {
-        return createRepeatingTrigger(unhandledDatex2MessagesImportJobDetail, 0);
+    public SimpleTriggerFactoryBean unhandledDatex2MessagesImportJobTrigger(final JobDetail unhandledDatex2MessagesImportJobDetail,
+                                                                            @Value("${unhandledDatex2MessagesImportJob.frequency}") final long frequency) {
+        return createRepeatingTrigger(unhandledDatex2MessagesImportJobDetail, frequency);
     }
 
     private static JobDetailFactoryBean createJobDetail(final Class jobClass) {
--- src/main/java/fi/livi/digitraffic/tie/conf/TriggerFactoryFactory.java
+++ src/main/java/fi/livi/digitraffic/tie/conf/TriggerFactoryFactory.java
@@ -18,12 +18,8 @@ public final class TriggerFactoryFactory {
         factoryBean.setJobDetail(jobDetail);
         // Delay first execution 5 seconds
         factoryBean.setStartDelay(5000L);
-        factoryBean.setRepeatInterval(repeatIntervalMs > 0 ? repeatIntervalMs : 1000*60);
-        if (repeatIntervalMs <= 0) {
-            factoryBean.setRepeatCount(SimpleTrigger.MISFIRE_INSTRUCTION_FIRE_NOW);
-        } else {
-            factoryBean.setRepeatCount(SimpleTrigger.REPEAT_INDEFINITELY);
-        }
+        factoryBean.setRepeatInterval(repeatIntervalMs);
+        factoryBean.setRepeatCount(SimpleTrigger.REPEAT_INDEFINITELY);
         // In case of misfire: The first misfired execution is run immediately, remaining are discarded.
         // Next execution happens after desired interval. Effectively the first execution time is moved to current time.
         factoryBean.setMisfireInstruction(SimpleTrigger.MISFIRE_INSTRUCTION_RESCHEDULE_NOW_WITH_REMAINING_REPEAT_COUNT);
