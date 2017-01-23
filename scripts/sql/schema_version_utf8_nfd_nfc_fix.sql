1e77c80f2b0e0741bd7b9f12d3e614a91ddcd98a
Ville Kähkönen <ville.kahkonen@solita.fi>
2017-01-23 14:57:13 +0200

    Fix sql script to use unicode characters in replace statement

--- scripts/sql/schema_version_utf8_nfd_nfc_fix.sql
+++ scripts/sql/schema_version_utf8_nfd_nfc_fix.sql
@@ -1,6 +1,2 @@
--- UTF-8 nfd
-UPDATE "schema_version" SET "description" = REPLACE("description", 'a?', 'a'), "script" = REPLACE("script", 'a?', 'a');
-UPDATE "schema_version" SET "description" = REPLACE("description", 'o?', 'o'), "script" = REPLACE("script", 'o?', 'o');
--- UTF-8 nfc
-UPDATE "schema_version" SET "description" = REPLACE("description", 'ä', 'a'), "script" = REPLACE("script", 'ä', 'a');
-UPDATE "schema_version" SET "description" = REPLACE("description", 'ö', 'o'), "script" = REPLACE("script", 'ö', 'o');
+UPDATE "schema_version" SET "description" = REPLACE("description", UNISTR('\00E4'), 'a'), "script" = REPLACE("script", UNISTR('\00E4'), 'a');
+UPDATE "schema_version" SET "description" = REPLACE("description", UNISTR('\00F6'), 'o'), "script" = REPLACE("script", UNISTR('\00F6'), 'o');
