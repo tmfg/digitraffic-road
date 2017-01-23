bd25eaf418953e164af5f87f543f59974d280f0d
Ville Kähkönen <ville.kahkonen@solita.fi>
2017-01-23 15:12:10 +0200

    Add replace statement that may or may not work in non *nix systems

--- scripts/sql/schema_version_utf8_nfd_nfc_fix.sql
+++ scripts/sql/schema_version_utf8_nfd_nfc_fix.sql
@@ -1,2 +1,5 @@
 UPDATE "schema_version" SET "description" = REPLACE("description", UNISTR('\00E4'), 'a'), "script" = REPLACE("script", UNISTR('\00E4'), 'a');
 UPDATE "schema_version" SET "description" = REPLACE("description", UNISTR('\00F6'), 'o'), "script" = REPLACE("script", UNISTR('\00F6'), 'o');
+
+UPDATE "schema_version" SET "description" = REPLACE("description", 'a?', 'a'), "script" = REPLACE("script", 'a?', 'a');
+UPDATE "schema_version" SET "description" = REPLACE("description", 'o?', 'o'), "script" = REPLACE("script", 'o?', 'o');
