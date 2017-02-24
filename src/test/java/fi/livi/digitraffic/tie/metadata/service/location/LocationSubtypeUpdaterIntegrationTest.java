b309b7b9131d8c538f1262b15d64ad58b78e2516
Teijo Rönkä <teijo.ronka@solita.fi>
2017-02-24 09:19:59 +0200

    DPO-25 test with broken data

--- src/test/java/fi/livi/digitraffic/tie/metadata/service/location/LocationSubtypeUpdaterIntegrationTest.java
+++ src/test/java/fi/livi/digitraffic/tie/metadata/service/location/LocationSubtypeUpdaterIntegrationTest.java
@@ -1,25 +1,41 @@
 package fi.livi.digitraffic.tie.metadata.service.location;
 
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertThat;
+
 import java.io.File;
 import java.nio.file.Path;
+import java.util.List;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
 import fi.livi.digitraffic.tie.base.AbstractMetadataIntegrationTest;
+import fi.livi.digitraffic.tie.metadata.model.location.LocationSubtype;
 
 public class LocationSubtypeUpdaterIntegrationTest extends AbstractMetadataIntegrationTest {
     @Autowired
     private LocationSubtypeUpdater locationSubtypeUpdater;
 
-    public static final String SUBTYPES_FILE_NAME = "/locations/SUBTYPES.DAT";
+    public static final String SUBTYPES_FILE_NAME_OK = "/locations/SUBTYPES.DAT";
+    public static final String SUBTYPES_FILE_NAME_ERROR = "/locations/SUBTYPES_ERROR.DAT";
 
     @Test
     @Transactional(readOnly = true)
-    public void testUpdateLocationSubtypes() {
-        final Path path = new File(getClass().getResource(SUBTYPES_FILE_NAME).getFile()).toPath();
+    public void updateLocationSubtypesOk() {
+        final Path path = new File(getClass().getResource(SUBTYPES_FILE_NAME_OK).getFile()).toPath();
 
-        locationSubtypeUpdater.updateLocationSubtypes(path, "VERSION");
+        final List<LocationSubtype> subtypes = locationSubtypeUpdater.updateLocationSubtypes(path, "VERSION");
+        assertThat(subtypes, hasSize(106));
+    }
+
+    @Test
+    @Transactional(readOnly = true)
+    public void updateLocationSubtypesError() {
+        final Path path = new File(getClass().getResource(SUBTYPES_FILE_NAME_ERROR).getFile()).toPath();
+
+        final List<LocationSubtype> subtypes = locationSubtypeUpdater.updateLocationSubtypes(path, "VERSION");
+        assertThat(subtypes, hasSize(1));
     }
 }
