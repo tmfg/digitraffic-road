f17b4f64a45320c781a30d959046d1be10467310
Ville Kähkönen <ville.kahkonen@solita.fi>
2017-03-02 15:37:28 +0200

    DPO-107 remove unused import

--- src/main/java/fi/livi/digitraffic/tie/data/service/DayDataService.java
+++ src/main/java/fi/livi/digitraffic/tie/data/service/DayDataService.java
@@ -10,7 +10,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 import fi.livi.digitraffic.tie.data.dao.DayDataRepository;
