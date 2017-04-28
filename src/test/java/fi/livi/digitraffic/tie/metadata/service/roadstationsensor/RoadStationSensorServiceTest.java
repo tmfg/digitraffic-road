31ac5c175468627c9c66ba4bcaefd3ea4c4de683
Jouni Sonninen <jouni.sonninen@solita.fi>
2017-04-28 13:50:23 +0300

    Debug for test

--- src/test/java/fi/livi/digitraffic/tie/metadata/service/roadstationsensor/RoadStationSensorServiceTest.java
+++ src/test/java/fi/livi/digitraffic/tie/metadata/service/roadstationsensor/RoadStationSensorServiceTest.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -15,18 +17,22 @@ import fi.livi.digitraffic.tie.metadata.model.RoadStationType;
 @RunWith(SpringJUnit4ClassRunner.class)
 public class RoadStationSensorServiceTest extends AbstractTest {
 
+    private static final Logger log = LoggerFactory.getLogger(RoadStationSensorServiceTest.class);
+
     @Autowired
     private RoadStationSensorService roadStationSensorService;
 
     @Test
     public void testFindAllNonObsoleteRoadStationSensors() {
         final List<RoadStationSensor> sensors = roadStationSensorService.findAllNonObsoleteRoadStationSensors(RoadStationType.WEATHER_STATION);
+        log.info("Sensors {}", sensors);
         Assert.assertTrue(sensors.size() >= 56);
     }
 
     @Test
     public void testFindRoadStationSensorsByRoadStationType() {
         List<RoadStationSensor> sensors = roadStationSensorService.findAllRoadStationSensors(RoadStationType.WEATHER_STATION);
+        log.info("Sensors {}", sensors);
         Assert.assertTrue(sensors.size() >= 56);
     }
 }
