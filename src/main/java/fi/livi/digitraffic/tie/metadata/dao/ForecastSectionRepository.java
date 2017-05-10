38aa923789b1771a76f22c927c47eccb36eb1186
Teijo Rönkä <teijo.ronka@solita.fi>
2017-05-10 10:01:42 +0300

    DPO-179 speed up forecast section fetching

--- src/main/java/fi/livi/digitraffic/tie/metadata/dao/ForecastSectionRepository.java
+++ src/main/java/fi/livi/digitraffic/tie/metadata/dao/ForecastSectionRepository.java
@@ -1,11 +1,21 @@
 package fi.livi.digitraffic.tie.metadata.dao;
 
+import java.util.List;
+
+import javax.persistence.QueryHint;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.stereotype.Repository;
 
 import fi.livi.digitraffic.tie.metadata.model.forecastsection.ForecastSection;
 
 @Repository
 public interface ForecastSectionRepository extends JpaRepository<ForecastSection, Long> {
-
+    @QueryHints(@QueryHint(name="org.hibernate.fetchSize", value="1000"))
+    @EntityGraph(attributePaths = { "road", "startRoadSection", "endRoadSection", "startRoadSection.roadDistrict", "endRoadSection" +
+        ".roadDistrict", "forecastSectionCoordinates" }, type = EntityGraph.EntityGraphType.LOAD)
+    List<ForecastSection> findAll(final Sort sort);
 }
