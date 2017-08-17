cc2667d4e8fd5d60afeb6e853c41ad424b84e0cd
Teijo Rönkä <teijo.ronka@solita.fi>
2017-08-17 13:04:35 +0300

    DPO-227 fixed test

--- src/test/java/fi/livi/digitraffic/tie/data/controller/DataUpdatedControllerRestWebTest.java
+++ src/test/java/fi/livi/digitraffic/tie/data/controller/DataUpdatedControllerRestWebTest.java
@@ -6,10 +6,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.apache.commons.lang3.reflect.FieldUtils;
 import org.hamcrest.Matchers;
 import org.junit.Test;
+import org.mockito.internal.util.collections.Sets;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,31 +29,36 @@ import fi.livi.digitraffic.tie.conf.MetadataApplicationConfiguration;
 public class DataUpdatedControllerRestWebTest extends AbstractRestWebTest {
     private static final Logger log = LoggerFactory.getLogger(DataUpdatedControllerRestWebTest.class);
 
+    private static final Set<String> excludedOperations = Sets.newSet(
+        "FLUENCY_HISTORY_DATA_PATH",
+        "TRAFFIC_DISORDERS_DATEX2_PATH",
+        "FORECAST_SECTION_WEATHER_DATA_PATH",
+        "ROADWORKS_DATEX2_PATH");
+
     @Autowired
     private DataController dataController;
 
     @Test
     public void testDataUpdatedExists() throws Exception {
+       final Field[] fieldArray = FieldUtils.getAllFields(DataController.class);
+       final List<Field> fields = Stream.of(fieldArray).filter(this::filter).collect(Collectors.toList());
 
-        final Field[] fields = FieldUtils.getAllFields(DataController.class);
-        for (final Field field : fields) {
-            if ( field.getName().endsWith("_PATH")
-                 && !field.getName().equals("FLUENCY_HISTORY_DATA_PATH")
-                 && !field.getName().equals("TRAFFIC_DISORDERS_DATEX2_PATH")
-                 && !field.getName().equals("FORECAST_SECTION_WEATHER_DATA_PATH")) {
+        for(final Field field : fields) {
+            final String url = MetadataApplicationConfiguration.API_V1_BASE_PATH +
+                         MetadataApplicationConfiguration.API_DATA_PART_PATH +
+                         field.get(dataController) +
+                         "?" + DataController.LAST_UPDATED_PARAM + "=true";
 
-                final String url = MetadataApplicationConfiguration.API_V1_BASE_PATH +
-                             MetadataApplicationConfiguration.API_DATA_PART_PATH +
-                             field.get(dataController) +
-                             "?" + DataController.LAST_UPDATED_PARAM + "=true";
-
-                log.info("Test url: " + url);
-                mockMvc.perform(get(url))
-                        .andExpect(status().isOk())
-                        .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-                        .andExpect(jsonPath("$.dataUpdatedTime", Matchers.notNullValue()));
-            }
+            log.info("Test url: " + url);
+            mockMvc.perform(get(url))
+                    .andExpect(status().isOk())
+                    .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                    .andExpect(jsonPath("$.dataUpdatedTime", Matchers.notNullValue()));
         }
+    }
+
+    private boolean filter(final Field f) {
+        return f.getName().endsWith("_PATH") && !excludedOperations.contains(f.getName());
     }
 
 }
