a3e44e551d5c93a536cf55c2e93c8bd8caddcae0
Teijo Rönkä <teijo.ronka@solita.fi>
2017-09-28 10:33:26 +0300

    DPO-227 merge-fixes

--- src/main/java/fi/livi/digitraffic/tie/data/controller/DataController.java
+++ src/main/java/fi/livi/digitraffic/tie/data/controller/DataController.java
@@ -97,7 +97,7 @@ public class DataController {
 
     @ApiOperation("Current fluency data of links including journey times")
     @RequestMapping(method = RequestMethod.GET, path = FLUENCY_CURRENT_PATH, produces = APPLICATION_JSON_UTF8_VALUE)
-    @ApiResponses({ @ApiResponse(code = 200, message = "Successful retrieval of current fluency data") })
+    @ApiResponses(@ApiResponse(code = 200, message = "Successful retrieval of current fluency data"))
     public TrafficFluencyRootDataObjectDto fluencyCurrent(
             @ApiParam("If parameter is given result will only contain update status.")
             @RequestParam(value=LAST_UPDATED_PARAM, required = false, defaultValue = "false") final
@@ -107,7 +107,7 @@ public class DataController {
 
     @ApiOperation("Current fluency data of link including journey times")
     @RequestMapping(method = RequestMethod.GET, path = FLUENCY_CURRENT_PATH + "/{id}", produces = APPLICATION_JSON_UTF8_VALUE)
-    @ApiResponses({ @ApiResponse(code = 200, message = "Successful retrieval of current fluency data") })
+    @ApiResponses(@ApiResponse(code = 200, message = "Successful retrieval of current fluency data"))
     public TrafficFluencyRootDataObjectDto fluencyCurrentById(
             @ApiParam(value = "Link id", required = true)
             @PathVariable
@@ -117,7 +117,7 @@ public class DataController {
 
     @ApiOperation("History data of links for previous day")
     @RequestMapping(method = RequestMethod.GET, path = FLUENCY_HISTORY_DAY_DATA_PATH, produces = APPLICATION_JSON_UTF8_VALUE)
-    @ApiResponses({ @ApiResponse(code = 200, message = "Successful retrieval of history data") })
+    @ApiResponses(@ApiResponse(code = 200, message = "Successful retrieval of history data"))
     public HistoryRootDataObjectDto fluencyHistoryPreviousDay(
             @ApiParam("If parameter is given result will only contain update status.")
             @RequestParam(value=LAST_UPDATED_PARAM, required = false, defaultValue = "false") final
@@ -127,7 +127,7 @@ public class DataController {
 
     @ApiOperation("History data of link for previous day")
     @RequestMapping(method = RequestMethod.GET, path = FLUENCY_HISTORY_DAY_DATA_PATH + "/{id}", produces = APPLICATION_JSON_UTF8_VALUE)
-    @ApiResponses({ @ApiResponse(code = 200, message = "Successful retrieval of history data") })
+    @ApiResponses(@ApiResponse(code = 200, message = "Successful retrieval of history data"))
     public HistoryRootDataObjectDto fluencyHistoryPreviousDayById(
             @ApiParam(value = "Link id", required = true)
             @PathVariable
@@ -137,7 +137,7 @@ public class DataController {
 
     @ApiOperation("History data of link for given month")
     @RequestMapping(method = RequestMethod.GET, path = FLUENCY_HISTORY_DATA_PATH + "/{id}", produces = APPLICATION_JSON_UTF8_VALUE)
-    @ApiResponses({ @ApiResponse(code = 200, message = "Successful retrieval of history data") })
+    @ApiResponses(@ApiResponse(code = 200, message = "Successful retrieval of history data"))
     public HistoryRootDataObjectDto fluencyHistoryById(
             @ApiParam(value = "Link id", required = true)
             @PathVariable
@@ -155,7 +155,7 @@ public class DataController {
 
     @ApiOperation("Current free flow speeds")
     @RequestMapping(method = RequestMethod.GET, path = FREE_FLOW_SPEEDS_PATH, produces = APPLICATION_JSON_UTF8_VALUE)
-    @ApiResponses({ @ApiResponse(code = 200, message = "Successful retrieval of free flow speeds") })
+    @ApiResponses(@ApiResponse(code = 200, message = "Successful retrieval of free flow speeds"))
     public FreeFlowSpeedRootDataObjectDto freeFlowSpeeds(
             @ApiParam("If parameter is given result will only contain update status")
             @RequestParam(value=LAST_UPDATED_PARAM, required = false, defaultValue = "false") final
@@ -165,7 +165,7 @@ public class DataController {
 
     @ApiOperation("Current free flow speeds of link")
     @RequestMapping(method = RequestMethod.GET, path = FREE_FLOW_SPEEDS_PATH + "/link/{id}", produces = APPLICATION_JSON_UTF8_VALUE)
-    @ApiResponses({ @ApiResponse(code = 200, message = "Successful retrieval of free flow speeds") })
+    @ApiResponses(@ApiResponse(code = 200, message = "Successful retrieval of free flow speeds"))
     public FreeFlowSpeedRootDataObjectDto freeFlowSpeedsOfLinkById(
             @ApiParam(value = "Link id", required = true)
             @PathVariable
@@ -175,7 +175,7 @@ public class DataController {
 
     @ApiOperation("Current free flow speeds of TMS station (Traffic Measurement System / LAM)")
     @RequestMapping(method = RequestMethod.GET, path = FREE_FLOW_SPEEDS_PATH + "/tms/{id}", produces = APPLICATION_JSON_UTF8_VALUE)
-    @ApiResponses({ @ApiResponse(code = 200, message = "Successful retrieval of free flow speeds")})
+    @ApiResponses(@ApiResponse(code = 200, message = "Successful retrieval of free flow speeds"))
     public FreeFlowSpeedRootDataObjectDto freeFlowSpeedsOfTmsById(
             @ApiParam(value = "TMS station id", required = true)
             @PathVariable
@@ -185,7 +185,7 @@ public class DataController {
 
     @ApiOperation("Current data of cameras")
     @RequestMapping(method = RequestMethod.GET, path = CAMERA_DATA_PATH, produces = APPLICATION_JSON_UTF8_VALUE)
-    @ApiResponses({ @ApiResponse(code = 200, message = "Successful retrieval of camera station data") })
+    @ApiResponses(@ApiResponse(code = 200, message = "Successful retrieval of camera station data"))
     public CameraRootDataObjectDto cameraData(
             @ApiParam("If parameter is given result will only contain update status.")
             @RequestParam(value=LAST_UPDATED_PARAM, required = false, defaultValue = "false") final
@@ -195,7 +195,7 @@ public class DataController {
 
     @ApiOperation("Current data of camera")
     @RequestMapping(method = RequestMethod.GET, path = CAMERA_DATA_PATH + "/{id}", produces = APPLICATION_JSON_UTF8_VALUE)
-    @ApiResponses({ @ApiResponse(code = 200, message = "Successful retrieval of camera station data") })
+    @ApiResponses(@ApiResponse(code = 200, message = "Successful retrieval of camera station data"))
     public CameraRootDataObjectDto cameraDataById(
             @ApiParam(value = "Camera id", required = true)
             @PathVariable
@@ -205,7 +205,7 @@ public class DataController {
 
     @ApiOperation("Current data of TMS Stations (Traffic Measurement System / LAM)")
     @RequestMapping(method = RequestMethod.GET, path = TMS_DATA_PATH, produces = APPLICATION_JSON_UTF8_VALUE)
-    @ApiResponses({ @ApiResponse(code = 200, message = "Successful retrieval of TMS Station data") })
+    @ApiResponses(@ApiResponse(code = 200, message = "Successful retrieval of TMS Station data"))
     public TmsRootDataObjectDto tmsData(
             @ApiParam("If parameter is given result will only contain update status.")
             @RequestParam(value=LAST_UPDATED_PARAM, required = false, defaultValue = "false") final
@@ -215,7 +215,7 @@ public class DataController {
 
     @ApiOperation("Current data of TMS station (Traffic Measurement System / LAM)")
     @RequestMapping(method = RequestMethod.GET, path = TMS_DATA_PATH + "/{id}", produces = APPLICATION_JSON_UTF8_VALUE)
-    @ApiResponses({ @ApiResponse(code = 200, message = "Successful retrieval of weather station data") })
+    @ApiResponses(@ApiResponse(code = 200, message = "Successful retrieval of weather station data"))
     public TmsRootDataObjectDto tmsDataById(
             @ApiParam(value = "TMS Station id", required = true)
             @PathVariable
@@ -225,7 +225,7 @@ public class DataController {
 
     @ApiOperation("Current data of Weather Stations")
     @RequestMapping(method = RequestMethod.GET, path = WEATHER_DATA_PATH, produces = APPLICATION_JSON_UTF8_VALUE)
-    @ApiResponses({ @ApiResponse(code = 200, message = "Successful retrieval of Weather Station data") })
+    @ApiResponses(@ApiResponse(code = 200, message = "Successful retrieval of Weather Station data"))
     public WeatherRootDataObjectDto weatherData(
             @ApiParam("If parameter is given result will only contain update status.")
             @RequestParam(value=LAST_UPDATED_PARAM, required = false, defaultValue = "false") final
@@ -235,7 +235,7 @@ public class DataController {
 
     @ApiOperation("Current data of Weather Station")
     @RequestMapping(method = RequestMethod.GET, path = WEATHER_DATA_PATH + "/{id}", produces = APPLICATION_JSON_UTF8_VALUE)
-    @ApiResponses({ @ApiResponse(code = 200, message = "Successful retrieval of weather station data") })
+    @ApiResponses(@ApiResponse(code = 200, message = "Successful retrieval of weather station data"))
     public WeatherRootDataObjectDto weatherDataById(
             @ApiParam(value = "Weather Station id", required = true)
             @PathVariable
@@ -245,7 +245,7 @@ public class DataController {
 
     @ApiOperation("Current data of Weather Forecast Sections")
     @RequestMapping(method = RequestMethod.GET, path = FORECAST_SECTION_WEATHER_DATA_PATH, produces = APPLICATION_JSON_UTF8_VALUE)
-    @ApiResponses({ @ApiResponse(code = 200, message = "Successful retrieval of Weather Forecast Section data") })
+    @ApiResponses(@ApiResponse(code = 200, message = "Successful retrieval of Weather Forecast Section data"))
     public ForecastSectionWeatherRootDto roadConditions(
             @ApiParam("If parameter is given result will only contain update status")
             @RequestParam(value=LAST_UPDATED_PARAM, required = false, defaultValue = "false") final
@@ -255,29 +255,28 @@ public class DataController {
 
     @ApiOperation("Active traffic disorders Datex2 messages")
     @RequestMapping(method = RequestMethod.GET, path = TRAFFIC_DISORDERS_DATEX2_PATH, produces = { APPLICATION_XML_VALUE, APPLICATION_JSON_UTF8_VALUE})
-    @ApiResponses(value = { @ApiResponse(code = 200, message = "Successful retrieval of traffic disorders") })
+    @ApiResponses(@ApiResponse(code = 200, message = "Successful retrieval of traffic disorders"))
     public TrafficDisordersDatex2Response trafficDisordersDatex2() {
-        return datex2DataService.findActiveDatex2Response();
+        return datex2DataService.findActiveTrafficDisorders();
     }
 
     @ApiOperation("Traffic disorder Datex2 messages by situation id")
     @RequestMapping(method = RequestMethod.GET, path = TRAFFIC_DISORDERS_DATEX2_PATH + "/{situationId}", produces = { APPLICATION_XML_VALUE, APPLICATION_JSON_UTF8_VALUE})
-    @ApiResponses(value = { @ApiResponse(code = 200, message = "Successful retrieval of traffic disorders"),
-                            @ApiResponse(code = 404, message = "Situation id not found") })
+    @ApiResponses({ @ApiResponse(code = 200, message = "Successful retrieval of traffic disorders"),
+                    @ApiResponse(code = 404, message = "Situation id not found") })
     public TrafficDisordersDatex2Response trafficDisordersDatex2BySituationId(
             @ApiParam(value = "Situation id.", required = true)
-            @PathVariable
-            String situationId) {
-        return datex2DataService.findAllDatex2ResponsesBySituationId(situationId);
+            @PathVariable final String situationId) {
+        return datex2DataService.getAllTrafficDisordersBySituationId(situationId);
     }
 
     @ApiOperation("Traffic disorder Datex2 messages disorders history")
     @RequestMapping(method = RequestMethod.GET, path = TRAFFIC_DISORDERS_DATEX2_PATH + "/history", produces = { APPLICATION_XML_VALUE, APPLICATION_JSON_UTF8_VALUE})
-    @ApiResponses(value = { @ApiResponse(code = 200, message = "Successful retrieval of traffic disorders"),
+    @ApiResponses(      {   @ApiResponse(code = 200, message = "Successful retrieval of traffic disorders"),
                             @ApiResponse(code = 400, message = "Invalid parameter"),
                             @ApiResponse(code = 404, message = "Situation id not found") })
     public TrafficDisordersDatex2Response trafficDisordersDatex2OfHistory(
-            @ApiParam(value = "Situation id")
+            @ApiParam("Situation id")
             @RequestParam(required = false)
             final String situationId,
             @ApiParam(value = "Year (>2014)", required = true)
@@ -286,6 +285,6 @@ public class DataController {
             @ApiParam(value = "Month (1-12)", required = true)
             @RequestParam @Range(min = 1, max = 12)
             final int month) {
-        return datex2DataService.findDatex2Responses(situationId, year, month);
+        return datex2DataService.findTrafficDisorders(situationId, year, month);
     }
 }
