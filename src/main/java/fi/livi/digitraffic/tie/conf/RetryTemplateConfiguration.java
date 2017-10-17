4b8ea7156b51560be64f20d9e6fddafc40063d87
Ville Kähkönen <ville.kahkonen@solita.fi>
2017-10-17 14:04:15 +0300

    Delete duplicate RetryTemplate configuration

--- src/main/java/fi/livi/digitraffic/tie/conf/RetryTemplateConfiguration.java
+++ src/main/java/fi/livi/digitraffic/tie/conf/RetryTemplateConfiguration.java
@@ -3,6 +3,7 @@ package fi.livi.digitraffic.tie.conf;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
 import org.springframework.retry.support.RetryTemplate;
 
 @Configuration
@@ -10,7 +11,11 @@ public class RetryTemplateConfiguration {
 
     @Bean
     public RetryTemplate retryTemplate() {
+        final SimpleRetryPolicy retryPolicy = new SimpleRetryPolicy();
+        retryPolicy.setMaxAttempts(5);
+
         final RetryTemplate retryTemplate = new RetryTemplate();
+        retryTemplate.setRetryPolicy(retryPolicy);
 
         final FixedBackOffPolicy backOffPolicy = new FixedBackOffPolicy();
         backOffPolicy.setBackOffPeriod(1000L);
