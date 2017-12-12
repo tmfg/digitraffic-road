c8e963b279ee68db7ed2290303e6b88c2785a11b
Teijo Rönkä <teijo.ronka@solita.fi>
2017-12-12 09:23:18 +0200

    DPO-227 change logging, don't remove all roadworks when updating

--- src/main/java/fi/livi/digitraffic/tie/data/service/datex2/Datex2RoadworksMessageUpdater.java
+++ src/main/java/fi/livi/digitraffic/tie/data/service/datex2/Datex2RoadworksMessageUpdater.java
@@ -43,8 +43,6 @@ public class Datex2RoadworksMessageUpdater {
     public void updateDatex2RoadworksMessages() {
         final String message = datex2RoadworksHttpClient.getRoadWorksMessage();
 
-        datex2Repository.removeAllRoadworks();
-
         datex2UpdateService.updateRoadworks(convert(message));
     }
 
@@ -61,7 +59,7 @@ public class Datex2RoadworksMessageUpdater {
         final long updatedCount = sp.getSituation().stream().filter(s -> isNewSituation(versionTimes.get(s.getId()), s)).count();
         final long newCount = sp.getSituation().stream().filter(s -> versionTimes.get(s.getId()) == null).count();
 
-        log.info("Updated {} situations, {} new situations", updatedCount, newCount);
+        log.info("situations.updated={}, situations.new={}", updatedCount, newCount);
 
         return sp.getSituation().stream()
             .filter(s -> isNewSituation(versionTimes.get(s.getId()), s))
