d8cf13002373b025328d79debc5c48b546f3990e
Teijo Rönkä <teijo.ronka@solita.fi>
2018-02-06 09:52:52 +0200

    DPO-337 schedule update

---
+++ src/main/java/fi/livi/digitraffic/tie/metadata/quartz/Datex2WeightLimitsMessageUpdateJob.java
@@ -0,0 +1,18 @@
+package fi.livi.digitraffic.tie.metadata.quartz;
+
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.JobExecutionContext;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import fi.livi.digitraffic.tie.data.service.datex2.Datex2SimpleMessageUpdater;
+
+@DisallowConcurrentExecution
+public class Datex2WeightLimitsMessageUpdateJob extends SimpleUpdateJob {
+    @Autowired
+    private Datex2SimpleMessageUpdater datex2SimpleMessageUpdater;
+
+    @Override
+    protected void doExecute(final JobExecutionContext context) throws Exception {
+        datex2SimpleMessageUpdater.updateDatex2WeightLimitationsMessages();
+    }
+}
