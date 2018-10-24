887eb677fd56f1fd046bd5cedd6e93e2062b58cc
Jouni <jouni.sonninen@solita.fi>
2018-10-24 11:09:03 +0200

    DPO-398 RoadStationin setObsolete ei ole setteri, vaan siinä on logiikkaa mukana

--- src/main/java/fi/livi/digitraffic/tie/metadata/model/RoadStation.java
+++ src/main/java/fi/livi/digitraffic/tie/metadata/model/RoadStation.java
@@ -175,7 +175,7 @@ public class RoadStation {
         return obsolete;
     }
 
-    public void setObsolete(boolean obsolete) {
+    public void setObsolete(final boolean obsolete) {
         this.obsolete = obsolete;
     }
 
