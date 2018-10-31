48310f32bb597baf1a412735d8d2aef972bc9a36
Jouni <jouni.sonninen@solita.fi>
2018-10-31 09:47:45 +0100

    DPO-469 HARJA - curl response info

--- src/test/resources/harja/post.sh
+++ src/test/resources/harja/post.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-curl --header "Content-Type: application/json" --request POST --data @seuranta.json http://localhost:9010/api/v1/maintenance/tracking/work_machine
+curl -i --header "Content-Type: application/json" --request POST --data @seuranta.json http://localhost:9010/api/v1/maintenance/tracking/work_machine
