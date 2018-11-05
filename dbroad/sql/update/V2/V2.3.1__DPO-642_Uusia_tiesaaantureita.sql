89c208788351b623f9d977c4258c956a352a3d17
Jouni <jouni.sonninen@solita.fi>
2018-11-05 13:26:40 +0100

    DPO-639 Uusia tiesääantureita - unused tgt removed

--- dbroad/sql/update/V2/V2.3.1__DPO-642_Uusia_tiesaaantureita.sql
+++ dbroad/sql/update/V2/V2.3.1__DPO-642_Uusia_tiesaaantureita.sql
@@ -1,5 +1,5 @@
 -- some new sensors to public
-INSERT INTO ALLOWED_ROAD_STATION_SENSOR AS tgt
+INSERT INTO ALLOWED_ROAD_STATION_SENSOR
   select nextval('SEQ_ALLOWED_SENSOR') as id, 48 as natural_id, 'WEATHER_STATION' as road_station_type union
   select nextval('SEQ_ALLOWED_SENSOR'), 73, 'WEATHER_STATION' union
   select nextval('SEQ_ALLOWED_SENSOR'), 37, 'WEATHER_STATION' union
