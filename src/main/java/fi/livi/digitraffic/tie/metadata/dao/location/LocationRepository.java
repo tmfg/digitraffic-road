2b2906f1aaf21351dd47224f8f3a1d02c7fb81f3
Teijo Rönkä <teijo.ronka@solita.fi>
2019-02-15 12:04:47 +0200

    DPO-732 try fetchsize 0

--- src/main/java/fi/livi/digitraffic/tie/metadata/dao/location/LocationRepository.java
+++ src/main/java/fi/livi/digitraffic/tie/metadata/dao/location/LocationRepository.java
@@ -1,5 +1,7 @@
 package fi.livi.digitraffic.tie.metadata.dao.location;
 
+import static org.hibernate.jpa.QueryHints.HINT_FETCH_SIZE;
+
 import java.util.List;
 import java.util.stream.Stream;
 import javax.persistence.QueryHint;
@@ -15,6 +17,6 @@ import fi.livi.digitraffic.tie.metadata.model.location.Location;
 public interface LocationRepository extends JpaRepository<Location, Integer> {
     LocationJson findByVersionAndLocationCode(final String version, final int locationCode);
 
-    @QueryHints(@QueryHint(name="org.hibernate.fetchSize", value="7000"))
+    @QueryHints(@QueryHint(name=HINT_FETCH_SIZE, value="0"))
     Stream<LocationJson> findAllByVersion(final String version);
 }
