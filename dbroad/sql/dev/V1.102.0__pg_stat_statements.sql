a06bdda82b7497be70281d74644bd74da4436bb8
Jouni <jouni.sonninen@solita.fi>
2019-03-14 08:53:06 +0100

    Added pghero stats history

--- dbroad/sql/dev/V1.102.0__pg_stat_statements.sql
+++ dbroad/sql/dev/V1.102.0__pg_stat_statements.sql
@@ -1,1 +1,13 @@
 CREATE EXTENSION IF NOT EXISTS pg_stat_statements WITH SCHEMA public;
+
+CREATE TABLE "pghero_query_stats" (
+  "id" serial primary key,
+  "database" text,
+  "user" text,
+  "query" text,
+  "query_hash" bigint,
+  "total_time" float,
+  "calls" bigint,
+  "captured_at" timestamp
+);
+CREATE INDEX ON "pghero_query_stats" ("database", "captured_at");
