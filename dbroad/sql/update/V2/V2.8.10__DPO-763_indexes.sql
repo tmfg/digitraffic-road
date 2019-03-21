05122d3f9fa9f873843ebd62e8b6758e289fbf9a
Jouni <jouni.sonninen@solita.fi>
2019-03-21 21:25:22 +0100

    DPO-763 Viimeisin muokkausaika pitää tallettaa kantaan data_updated-tauluun eikä aina tulkita muista tauluista - drop indexes for test

--- dbroad/sql/update/V2/V2.8.10__DPO-763_indexes.sql
+++ dbroad/sql/update/V2/V2.8.10__DPO-763_indexes.sql
@@ -7,3 +7,9 @@ CREATE INDEX publishable_tms_sensor_lotju_id_i
   ON road_station_sensor
   USING BTREE (lotju_id ASC)
   WHERE publishable = true AND road_station_sensor.road_station_type = 'TMS_STATION';
+
+-- test index drops
+drop index if exists sensor_value_measured;
+drop index if exists sensor_value_measured_i_test2;
+drop index if exists sensor_value_road_station_fk_i;
+drop index if exists sensor_value_search_u;
