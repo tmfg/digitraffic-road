1ecc305fc836524f4d9d1e14523c9d0c4cbf1cf0
Teijo Rönkä <teijo.ronka@solita.fi>
2019-06-25 14:45:03 +0300

    Revert "DPO-813 reindex and index rewrites"
    This reverts commit 890c681c251930526385ff0c62b8bea5f70cc94b.

--- dbroad/sql/update/V2/V2.10.0__indexes.sql
+++
@@ -1,9 +0,0 @@
-drop index camera_preset_publishable_i; -- unused
-
--- rewrite as partial index
-drop index camera_preset_id_ui;
-create index camera_preset_id_key on camera_preset(id) where obsolete_date is null;
-
--- slow ones
-reindex table sensor_value;
-reindex table work_machine_tracking;
--- dbroad/sql/update/beforeMigrate.sql
+++
@@ -1,9 +0,0 @@
--- reindex with every db update
-reindex table locking_table;
-reindex table camera_preset;
-reindex table data_updated;
-reindex table forecast_section_weather;
-reindex table datex2;
-reindex table datex2_situation;
-reindex table datex2_situation_record;
-reindex table road_station;
