97dd96720606f27410f90721758362d9762539d8
Teijo Rönkä <teijo.ronka@solita.fi>
2019-08-12 08:25:20 +0300

    fix test

--- src/test/java/fi/livi/digitraffic/tie/metadata/service/location/MetadataFileFetcherIntegrationTest.java
+++ src/test/java/fi/livi/digitraffic/tie/metadata/service/location/MetadataFileFetcherIntegrationTest.java
@@ -2,26 +2,19 @@ package fi.livi.digitraffic.tie.metadata.service.location;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.file.Path;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @Import(MetadataFileFetcher.class)
 public class MetadataFileFetcherIntegrationTest {
-    @Value("${metadata.tmc.url}")
-    private String tmcUrl;
-
     @Test(expected = FileNotFoundException.class)
     public void testLocationNotFound() throws IOException {
-        final MetadataFileFetcher fetcher = new MetadataFileFetcher("http://localhost/not_found");
+        final MetadataFileFetcher fetcher = new MetadataFileFetcher("https://aineistot.vayla.fi/not_found");
 
-        final Path path = fetcher.getLocationsFile(new MetadataVersions.MetadataVersion("", ""));
-        Assert.assertNotNull(path);
+        fetcher.getLocationsFile(new MetadataVersions.MetadataVersion("", ""));
     }
 }
