095c1ee7af44207525b09e226071effe50073385
Teijo Rönkä <teijo.ronka@solita.fi>
2019-09-13 10:33:26 +0300

    DPO-864 better test

--- src/test/java/fi/livi/digitraffic/tie/data/controller/VariableSignControllerTest.java
+++ src/test/java/fi/livi/digitraffic/tie/data/controller/VariableSignControllerTest.java
@@ -24,6 +24,16 @@ public class VariableSignControllerTest extends AbstractRestWebTest {
         return mockMvc.perform(get);
     }
 
+    private void insertTestData() {
+        entityManager.createNativeQuery(
+            "insert into device(id,updated_date,type,road_address,etrs_tm35fin_x,etrs_tm35fin_y,direction,carriageway) " +
+            "values('ID1',current_timestamp, 'NOPEUSRAJOITUS', '1 2 3',10, 20,'KASVAVA', 'NORMAALI');").executeUpdate();
+
+        entityManager.createNativeQuery(
+            "insert into device_data(created_date,device_id,display_value,additional_information,effect_date,cause,reliability) " +
+                "values (current_timestamp,'ID1','80',null,current_date,null,'NORMAALI');").executeUpdate();
+    }
+
     @Test
     public void noData() throws Exception {
         getJson(BetaController.VARIABLE_SIGNS_DATA_PATH)
@@ -36,5 +46,15 @@ public class VariableSignControllerTest extends AbstractRestWebTest {
     public void notExists() throws Exception {
         getJson(BetaController.VARIABLE_SIGNS_DATA_PATH + "/unknown")
             .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void deviceWithData() throws Exception {
+        insertTestData();
+
+        getJson(BetaController.VARIABLE_SIGNS_DATA_PATH + "/ID1")
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("features", Matchers.hasSize(1)))
+            .andExpect(jsonPath("features[0].properties.displayInformation", Matchers.equalTo("80")));
     }
 }
