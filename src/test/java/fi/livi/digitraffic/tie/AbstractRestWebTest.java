6cc8f02982fed8c0b2b34829cc78328d8604d2df
Teijo Rönkä <teijo.ronka@solita.fi>
2019-09-13 11:42:13 +0300

    fix property name

--- src/test/java/fi/livi/digitraffic/tie/AbstractRestWebTest.java
+++ src/test/java/fi/livi/digitraffic/tie/AbstractRestWebTest.java
@@ -14,7 +14,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 @TestPropertySource(properties = {
-    "marine.datasource.hikari.maximum-pool-size=1",
+    "road.datasource.hikari.maximum-pool-size=1",
 })
 public abstract class AbstractRestWebTest extends AbstractTest {
 
