e58992a736be97bfd129da4b8ec8d132de67bccd
Teijo Rönkä <teijo.ronka@solita.fi>
2019-09-13 15:02:51 +0300

    fix pool size

--- src/test/java/fi/livi/digitraffic/tie/AbstractRestWebTest.java
+++ src/test/java/fi/livi/digitraffic/tie/AbstractRestWebTest.java
@@ -14,7 +14,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 @TestPropertySource(properties = {
-    "road.datasource.hikari.maximum-pool-size=1",
+    "road.datasource.hikari.maximum-pool-size=2",
 })
 public abstract class AbstractRestWebTest extends AbstractTest {
 
