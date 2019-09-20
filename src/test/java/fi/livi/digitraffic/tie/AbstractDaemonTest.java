67fc4ca62883c818b76c397b74652e7cb3ffd888
Jouni Sonninen <jouni.sonninen@solita.fi>
2019-09-20 09:40:13 +0200

    DPO-718 Kamerakuvat S3 ämpäriin - Merge fixes

--- src/test/java/fi/livi/digitraffic/tie/AbstractDaemonTest.java
+++ src/test/java/fi/livi/digitraffic/tie/AbstractDaemonTest.java
@@ -1,9 +1,7 @@
 package fi.livi.digitraffic.tie;
 
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest(classes = RoadApplication.class)
 @TestPropertySource(properties = {
     "app.type=daemon",
     "spring.main.web-application-type=none",
