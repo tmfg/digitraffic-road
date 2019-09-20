6e18c2e20507b16ee950f0cd6063bd9c94a5d54b
Teemu Lukkarinen <teemu.lukkarinen@solita.fi>
2019-09-20 13:13:17 +0300

    lokien koot

--- src/main/java/fi/livi/digitraffic/tie/conf/LoggerDebugScheduler.java
+++ src/main/java/fi/livi/digitraffic/tie/conf/LoggerDebugScheduler.java
@@ -23,17 +23,56 @@ public class LoggerDebugScheduler {
 
     @Scheduled(fixedDelayString = "60000")
     public void debugLog() {
-        try {
-            File tmp = new File("/logs");
-            log.info("mount free space: " + (tmp.getFreeSpace() / 1024 / 1024) + "M");
-        } catch (Exception e) {
-            log.warn("mount check failed", e);
-        }
-
         try (ReversedLinesFileReader rReader = new ReversedLinesFileReader(new File("logs/road-daemon-daily.log"))) {
             log.info("last line: {}", rReader.readLine());
         } catch (Exception e) {
             log.warn("log check failed", e);
         }
+
+        try {
+            File tmp = new File("logs");
+
+            StringBuilder builder = new StringBuilder();
+
+            long total = loopDirectory(tmp, builder);
+
+            builder.append("mount free space: " + (tmp.getFreeSpace() / 1024 / 1024) + "M, logs: " + (total  / 1024 / 1024) + "M");
+
+            log.info(builder.toString());
+        } catch (Exception e) {
+            log.warn("mount check failed", e);
+        }
+    }
+
+    private long loopDirectory(File file, StringBuilder builder) {
+        long size = 0;
+
+        if (file.isDirectory()) {
+            for (File innerFile : file.listFiles()) {
+                if (innerFile.isFile()) {
+                    size += append(file, innerFile, builder);
+                } else if (innerFile.isDirectory()) {
+                    size += loopDirectory(innerFile, builder);
+                }
+            }
+        } else {
+            size += append(null, file, builder);
+        }
+
+        return size;
+    }
+
+    private long append(File parent, File file, StringBuilder builder) {
+        if (parent != null) {
+            builder.append(parent.getName());
+            builder.append("/");
+        }
+
+        builder.append(file.getName());
+        builder.append(": ");
+        builder.append(file.length());
+        builder.append("\n");
+
+        return file.length();
     }
 }
