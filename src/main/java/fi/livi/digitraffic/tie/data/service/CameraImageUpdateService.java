df570119b9ccbd4f9a2d4081ab5e59853e3d707d
Jouni Sonninen <jouni.sonninen@solita.fi>
2019-09-24 11:19:15 +0200

    DPO-718 Kamerakuvat S3 ämpäriin - TODO for publi/secret image hangling

--- src/main/java/fi/livi/digitraffic/tie/data/service/CameraImageUpdateService.java
+++ src/main/java/fi/livi/digitraffic/tie/data/service/CameraImageUpdateService.java
@@ -106,6 +106,8 @@ public class CameraImageUpdateService {
 
         // If preset exists in db, update image
         if (cameraPreset != null) {
+            // TODO DPO-462 get start time of public / not public state and write image public/secret acordingly
+            // Ie. cameraPreset.getRoadStation().isPublicNow()
             final boolean roadStationPublic = cameraPreset.getRoadStation().isPublic();
             final boolean isResultPublic = kuva.getJulkinen() && roadStationPublic;
             final ImageUpdateInfo transferInfo = transferKuva(kuva, presetId, filename, isResultPublic);
