bb25ec064ea9556d4bf629712ab91014e1f76d8c
Jouni Sonninen <jouni.sonninen@solita.fi>
2019-10-10 16:12:27 +0200

    DPO-462 Kelikamerakuvien salassapidon toteutus - more testing

--- src/test/java/fi/livi/digitraffic/tie/metadata/quartz/CameraStationPublicityUpdateJobTest.java
+++ src/test/java/fi/livi/digitraffic/tie/metadata/quartz/CameraStationPublicityUpdateJobTest.java
@@ -1,21 +1,23 @@
 package fi.livi.digitraffic.tie.metadata.quartz;
 
-import static fi.livi.ws.wsdl.lotju.kamerametatiedot._2018._06._15.JulkisuusTaso.*;
+import static fi.livi.ws.wsdl.lotju.kamerametatiedot._2018._06._15.JulkisuusTaso.JULKINEN;
+import static fi.livi.ws.wsdl.lotju.kamerametatiedot._2018._06._15.JulkisuusTaso.VALIAIKAISESTI_SALAINEN;
+import static java.lang.Thread.sleep;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.mockito.internal.verification.VerificationModeFactory.times;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import javax.xml.datatype.DatatypeConfigurationException;
@@ -26,6 +28,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.transaction.TestTransaction;
 
 import fi.livi.digitraffic.tie.AbstractDaemonTestWithoutS3;
 import fi.livi.digitraffic.tie.metadata.model.CameraPreset;
@@ -54,94 +57,152 @@ public class CameraStationPublicityUpdateJobTest extends AbstractDaemonTestWitho
     private LotjuCameraStationMetadataClient lotjuCameraStationMetadataClient;
 
     @Test
-    public void publicityChangeNow() throws KameraPerustiedotException, DatatypeConfigurationException {
+    public void publicityChangeNow() throws KameraPerustiedotException, DatatypeConfigurationException, InterruptedException {
         // Two cameras, change only first camera: public -> secret -> public, should affect right away
+        // Ohter camera should stay a same all the time
 
-        // PUBLIC
+        // Public in past -> valid now
         final Instant initialPublicFrom = getInstant(-60);
         final KameraVO kamera = createKamera(initialPublicFrom); // update only this camera
         final KameraVO kameraOther = createKamera(initialPublicFrom);
         List<EsiasentoVO> esiasentos = createEsiasentos(kamera.getId(), 2);
         List<EsiasentoVO> esiasentosOther = createEsiasentos(kameraOther.getId(), 1);
+        final Map<KameraVO, List<EsiasentoVO>> kameras = new HashMap<>();
+        kameras.put(kamera, esiasentos);
+        kameras.put(kameraOther, esiasentosOther);
+        updateCameraMetadataAndVerifyLotjuCalls(kameras, 1);
 
-        when(lotjuCameraStationMetadataClient.getKameras()).thenReturn(Arrays.asList(kamera, kameraOther));
-        when(lotjuCameraStationMetadataClient.getEsiasentos(kamera.getId())).thenReturn(esiasentos);
-        when(lotjuCameraStationMetadataClient.getEsiasentos(kameraOther.getId())).thenReturn(esiasentosOther);
-        when(lotjuCameraStationMetadataClient.getServerAddress()).thenReturn("http://LOTJU/KameraPerustiedot");
-        cameraStationUpdater.updateCameras();
-        entityManager.flush();
-        verify(lotjuCameraStationMetadataClient, times(1)).getKameras();
-        verify(lotjuCameraStationMetadataClient, times(1)).getEsiasentos(eq(kamera.getId()));
-        verify(lotjuCameraStationMetadataClient, times(1)).getEsiasentos(eq(kameraOther.getId()));
-        checkCameraPresetRoasStation(esiasentos.get(0).getId(), true, initialPublicFrom, true, true);
-        checkCameraPresetRoasStation(esiasentos.get(1).getId(), true, initialPublicFrom, true, true);
-        checkCameraPresetRoasStation(esiasentosOther.get(0).getId(), true, initialPublicFrom, true, true);
+        checkCameraPresetRoadStationPublicity(esiasentos.get(0).getId(), true, initialPublicFrom, true, true);
+        checkCameraPresetRoadStationPublicity(esiasentos.get(1).getId(), true, initialPublicFrom, true, true);
+        checkCameraPresetRoadStationPublicity(esiasentosOther.get(0).getId(), true, initialPublicFrom, true, true);
+        checkAllPublishableCameraPresetsContainsOnly(esiasentos.get(0).getId(), esiasentos.get(1).getId(), esiasentosOther.get(0).getId());
 
-        // PUBLIC -> SECRET
-        final Instant secretFrom = getInstant(0);
+        // Public -> secret in past -> valid now
+        final Instant secretFrom = getInstant(-10);
         kamera.setJulkisuus(createKameraJulkisuus(secretFrom, VALIAIKAISESTI_SALAINEN));
-        when(lotjuCameraStationMetadataClient.getKameras()).thenReturn(Collections.singletonList(kamera));
-        cameraStationUpdater.updateCameras();
-        checkCameraPresetRoasStation(esiasentos.get(0).getId(), false, secretFrom, true, false);
-        checkCameraPresetRoasStation(esiasentos.get(1).getId(), false, secretFrom, true, false);
-        checkCameraPresetRoasStation(esiasentosOther.get(0).getId(), true, initialPublicFrom, true, true);
+        updateCameraMetadataAndVerifyLotjuCalls(kameras, 2);
 
-        // SECRET -> PUBLIC
+        checkCameraPresetRoadStationPublicity(esiasentos.get(0).getId(), false, secretFrom, true, false);
+        checkCameraPresetRoadStationPublicity(esiasentos.get(1).getId(), false, secretFrom, true, false);
+        checkCameraPresetRoadStationPublicity(esiasentosOther.get(0).getId(), true, initialPublicFrom, true, true);
+        checkAllPublishableCameraPresetsContainsOnly(esiasentosOther.get(0).getId());
+
+        // Secret -> public now
         final Instant publicFrom = getInstant(0);
         kamera.setJulkisuus(createKameraJulkisuus(publicFrom, JULKINEN));
-        when(lotjuCameraStationMetadataClient.getKameras()).thenReturn(Collections.singletonList(kamera));
-        cameraStationUpdater.updateCameras();
-        checkCameraPresetRoasStation(esiasentos.get(0).getId(), true, publicFrom, false, true);
-        checkCameraPresetRoasStation(esiasentos.get(1).getId(), true, publicFrom, false, true);
-        checkCameraPresetRoasStation(esiasentosOther.get(0).getId(), true, initialPublicFrom, true, true);
+        updateCameraMetadataAndVerifyLotjuCalls(kameras, 3);
+
+        checkCameraPresetRoadStationPublicity(esiasentos.get(0).getId(), true, publicFrom, false, true);
+        checkCameraPresetRoadStationPublicity(esiasentos.get(1).getId(), true, publicFrom, false, true);
+        checkCameraPresetRoadStationPublicity(esiasentosOther.get(0).getId(), true, initialPublicFrom, true, true);
+        checkAllPublishableCameraPresetsContainsOnly(esiasentos.get(0).getId(), esiasentos.get(1).getId(), esiasentosOther.get(0).getId());
     }
 
     @Test
-    public void publicityChangeInFuture() throws KameraPerustiedotException, DatatypeConfigurationException {
-        // Cchange camera: public -> secret (in future)
+    public void publicityChangeInFuture() throws KameraPerustiedotException, DatatypeConfigurationException, InterruptedException {
+
+        // Create now public camera with presets
         final Instant publicFrom = getInstant(-60);
         final KameraVO kamera = createKamera(publicFrom);
-        List<EsiasentoVO> esiasentos = createEsiasentos(kamera.getId(), 2);
+        final List<EsiasentoVO> esiasentos = createEsiasentos(kamera.getId(), 2);
+        final Map<KameraVO, List<EsiasentoVO>> kameras = new HashMap<>();
+        kameras.put(kamera, esiasentos);
+        updateCameraMetadataAndVerifyLotjuCalls(kameras, 1);
 
-        when(lotjuCameraStationMetadataClient.getKameras()).thenReturn(Collections.singletonList(kamera));
-        when(lotjuCameraStationMetadataClient.getEsiasentos(kamera.getId())).thenReturn(esiasentos);
-        when(lotjuCameraStationMetadataClient.getServerAddress()).thenReturn("http://LOTJU/KameraPerustiedot");
-        cameraStationUpdater.updateCameras();
-        entityManager.flush();
-        verify(lotjuCameraStationMetadataClient, times(1)).getKameras();
-        verify(lotjuCameraStationMetadataClient, times(1)).getEsiasentos(eq(kamera.getId()));
-        checkCameraPresetRoasStation(esiasentos.get(0).getId(), true, publicFrom, true, true);
-        checkCameraPresetRoasStation(esiasentos.get(1).getId(), true, publicFrom, true, true);
+        // Check presets are public
+        checkCameraPresetRoadStationPublicity(esiasentos.get(0).getId(), true, publicFrom, true, true);
+        checkCameraPresetRoadStationPublicity(esiasentos.get(1).getId(), true, publicFrom, true, true);
+        checkAllPublishableCameraPresetsContainsOnly(esiasentos.get(0).getId(), esiasentos.get(1).getId());
 
-        // PUBLIC -> SECRET in future -> No change to curren publicity
-        final Instant secretFrom = getInstant(60);
+        // Public -> secret in future -> No change to current publicity
+        final Instant secretFrom = getInstant(2);
         kamera.setJulkisuus(createKameraJulkisuus(secretFrom, VALIAIKAISESTI_SALAINEN));
-        when(lotjuCameraStationMetadataClient.getKameras()).thenReturn(Collections.singletonList(kamera));
-        cameraStationUpdater.updateCameras();
-        checkCameraPresetRoasStation(esiasentos.get(0).getId(), true, secretFrom, true, false);
-        checkCameraPresetRoasStation(esiasentos.get(1).getId(), true, secretFrom, true, false);
+        updateCameraMetadataAndVerifyLotjuCalls(kameras, 2);
 
+        // At current time, road RoadStation is still public
+        checkCameraPresetRoadStationPublicity(esiasentos.get(0).getId(), true, secretFrom, true, false);
+        checkCameraPresetRoadStationPublicity(esiasentos.get(1).getId(), true, secretFrom, true, false);
+        checkAllPublishableCameraPresetsContainsOnly(esiasentos.get(0).getId(), esiasentos.get(1).getId());
+
+        // Wait for secretFrom time to pass -> RoadStation changes to not public
+        while ( Instant.now().isBefore(secretFrom) ) {
+            sleep(200);
+        }
+
+        // At current time, road RoadStation is not public as secretFrom time has passed
+        checkCameraPresetRoadStationPublicity(esiasentos.get(0).getId(), false, secretFrom, true, false);
+        checkCameraPresetRoadStationPublicity(esiasentos.get(1).getId(), false, secretFrom, true, false);
+        checkAllPublishableCameraPresetsContainsOnly();
     }
 
-    private RoadStation checkCameraPresetRoasStation(final Long lotjuId, final boolean isPublicNow, final Instant publicityStart,
-                                                     final boolean previousIsPublic, final boolean isPublic) {
-        final RoadStation rs = checkCameraPresetRoasStation(lotjuId, isPublicNow, publicityStart);
+    /**
+     *
+     * @param lotjuId preset lotjuId to check
+     * @param isPublicNow should roadstation be public now
+     * @param publicityStart publicity start time
+     * @param previousIsPublic what should previous value be
+     * @param isPublic what should isPublic value to be
+     * @return
+     */
+    private RoadStation checkCameraPresetRoadStationPublicity(final Long lotjuId, final boolean isPublicNow, final Instant publicityStart,
+                                                              final boolean previousIsPublic, final boolean isPublic) {
+        final CameraPreset cp = cameraPresetService.findCameraPresetByLotjuId(lotjuId);
+        final RoadStation rs = cp.getRoadStation();
+        assertEquals(publicityStart, rs.getPublicityStartTime().toInstant());
+        Assert.assertEquals(isPublicNow, rs.isPublicNow());
         Assert.assertEquals(previousIsPublic, rs.isPublicPrevious());
         Assert.assertEquals(isPublic, rs.isPublic());
         return rs;
     }
 
-    private RoadStation checkCameraPresetRoasStation(final Long lotjuId, final boolean isPublicNow, final Instant publicityStart) {
-        final RoadStation rs = checkCameraPresetRoasStation(lotjuId, isPublicNow);
-        assertEquals(publicityStart, rs.getPublicityStartTime().toInstant());
-        return rs;
+    private void checkAllPublishableCameraPresetsContainsOnly(long ... lotjuIds) {
+        // End current transaction and starts new as query uses current_timestamp from db
+        // and it is same as transaction start time.
+        endTransactionAndStartNew();
+        // uses current_timestamp
+        final List<CameraPreset> allPublishable = cameraPresetService.findAllPublishableCameraPresets();
+        final List<Long> publishableLotjuIds = allPublishable.stream().map(CameraPreset::getLotjuId).collect(Collectors.toList());
+        for (long lotjuId : lotjuIds) {
+            Assert.assertTrue(publishableLotjuIds.contains(lotjuId));
+        }
+        Assert.assertEquals(lotjuIds.length, publishableLotjuIds.size());
     }
 
-    private RoadStation checkCameraPresetRoasStation(Long lotjuId, final boolean isPublicNow) {
-        final CameraPreset cp = cameraPresetService.findCameraPresetByLotjuId(lotjuId);
-        final RoadStation rs = cp.getRoadStation();
-        Assert.assertEquals(isPublicNow, rs.isPublicNow());
-        return rs;
+    /**
+     *
+     * @param kameras kameras returned by lotju
+     * @param times how many times lotju should have been called
+     * @throws InterruptedException
+     */
+    private void updateCameraMetadataAndVerifyLotjuCalls(final Map<KameraVO, List<EsiasentoVO>> kameras, final int times) {
+        when(lotjuCameraStationMetadataClient.getKameras()).thenReturn(new ArrayList<>(kameras.keySet()));
+        kameras.entrySet().forEach(e -> {
+            when(lotjuCameraStationMetadataClient.getEsiasentos(e.getKey().getId())).thenReturn(e.getValue());
+        });
+        // Update cameras from lotju
+        runUpdateCameraMetadataJob();
+
+        // Verify lotju calls
+        verify(lotjuCameraStationMetadataClient, times(times)).getKameras();
+        kameras.keySet().forEach(kamera -> {
+            verify(lotjuCameraStationMetadataClient, times(times)).getEsiasentos(eq(kamera.getId()));
+        });
+    }
+
+    private void runUpdateCameraMetadataJob() {
+        cameraStationUpdater.updateCameras();
+        // Some data is read with native query and data mut be flushed to db
+        entityManager.flush();
+    }
+
+    /**
+     * Ends current transaction and starts new. Needed when using current_timestamp from db
+     * as it is same as transaction start time.
+     */
+    private void endTransactionAndStartNew() {
+        TestTransaction.flagForCommit();
+        TestTransaction.end();
+        TestTransaction.start();
     }
 
     private List<EsiasentoVO> createEsiasentos(final long kameraId, final int count) {
