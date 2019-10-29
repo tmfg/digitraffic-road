14620f6a1ea6f606175e8a58207c31940143b5ec
Teijo Rönkä <teijo.ronka@solita.fi>
2019-10-29 10:16:47 +0200

    DPO-713 DirectChannel back

--- src/main/java/fi/livi/digitraffic/tie/conf/MqttConfig.java
+++ src/main/java/fi/livi/digitraffic/tie/conf/MqttConfig.java
@@ -10,7 +10,6 @@ import org.springframework.integration.annotation.IntegrationComponentScan;
 import org.springframework.integration.annotation.MessagingGateway;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.channel.DirectChannel;
-import org.springframework.integration.channel.QueueChannel;
 import org.springframework.integration.config.EnableIntegration;
 import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
 import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
@@ -53,7 +52,7 @@ public class MqttConfig {
 
     @Bean
     public MessageChannel mqttOutboundChannel() {
-        return new QueueChannel();
+        return new DirectChannel();
     }
 
     @MessagingGateway(defaultRequestChannel = "mqttOutboundChannel", defaultRequestTimeout = "2000", defaultReplyTimeout = "2000")
