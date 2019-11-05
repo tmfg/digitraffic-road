35488c45fbfd1ec36b384e5ca7f1007a02b5cc0e
Teijo Rönkä <teijo.ronka@solita.fi>
2019-11-05 09:20:11 +0200

    DPO-864 fix

--- src/main/java/fi/livi/digitraffic/tie/metadata/dao/CodeDescriptionRepository.java
+++ src/main/java/fi/livi/digitraffic/tie/metadata/dao/CodeDescriptionRepository.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Repository;
 import fi.livi.digitraffic.tie.metadata.dto.CodeDescriptionJson;
 
 @Repository
-@ConditionalOnWebApplication
 public interface CodeDescriptionRepository extends SqlRepository {
     @Query(value = "select code, description from code_description where domain = 'VARIABLE_SIGN'", nativeQuery = true)
     List<CodeDescriptionJson> listAllVariableSignTypes();
