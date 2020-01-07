29c76e751e0c37346dae54a4c1676468e4083a17
Jouni Sonninen <jouni.sonninen@solita.fi>
2020-01-07 11:31:01 +0100

    DPO-949 Kelikamerakuvien historia julkiseksi: JMS listenter connection fix

--- src/main/java/fi/livi/digitraffic/tie/conf/jms/CameraMetadataJMSListenerConfiguration.java
+++ src/main/java/fi/livi/digitraffic/tie/conf/jms/CameraMetadataJMSListenerConfiguration.java
@@ -27,9 +27,9 @@ public class CameraMetadataJMSListenerConfiguration extends AbstractJMSListenerC
     private final Jaxb2Marshaller jaxb2Marshaller;
 
     @Autowired
-    public CameraMetadataJMSListenerConfiguration(@Qualifier("sonjaTestJMSConnectionFactory") QueueConnectionFactory connectionFactory,
-                                                  @Value("${jms.test.userId}") final String jmsUserId,
-                                                  @Value("${jms.test.password}") final String jmsPassword,
+    public CameraMetadataJMSListenerConfiguration(@Qualifier("sonjaJMSConnectionFactory") QueueConnectionFactory connectionFactory,
+                                                  @Value("${jms.userId}") final String jmsUserId,
+                                                  @Value("${jms.password}") final String jmsPassword,
                                                   @Value("#{'${jms.camera.meta.inQueue}'.split(',')}")final List<String> jmsQueueKeys,
                                                   final CameraMetadataMessageHandler cameraMetadataMessageHandler,
                                                   final LockingService lockingService,
