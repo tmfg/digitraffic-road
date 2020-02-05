bd1523c688865aa0133942ea7886de6f010462ca
Teijo Rönkä <teijo.ronka@solita.fi>
2020-02-05 13:48:20 +0200

    More sign values

---
+++ dbroad/sql/update/V2/V2.33.0__Variable_sign_codes.sql
@@ -0,0 +1,13 @@
+insert into code_description(domain, code, description_fi,description_en)
+    values ('VARIABLE_SIGN', '141', 'Epätasainen tie', 'Uneven road');
+insert into code_description(domain, code, description_fi,description_en)
+    values ('VARIABLE_SIGN', '143', 'Irtokiviä', 'Loose gravel');
+insert into code_description(domain, code, description_fi,description_en)
+    values ('VARIABLE_SIGN', '147', 'Vaarallinen tien reuna', 'Dangerous shoulders');
+insert into code_description(domain, code, description_fi,description_en)
+    values ('VARIABLE_SIGN', '154', 'Hiihtolatu', 'Ski track');
+insert into code_description(domain, code, description_fi,description_en)
+    values ('VARIABLE_SIGN', '155', 'Hirvieläimiä', 'Elks');
+insert into code_description(domain, code, description_fi,description_en)
+    values ('VARIABLE_SIGN', '156', 'Poroja', 'Raindeer');
+
