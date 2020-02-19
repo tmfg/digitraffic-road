7b88351c2a9ce3c66de45d29bb9df63d0cdbd800
Jouni Sonninen <jouni.sonninen@solita.fi>
2020-02-19 13:53:56 +0100

    DPO-631 HARJA työkoneen toteumatietojen haku ja tallennus digitrafficiin - Review fixes

--- src/main/java/fi/livi/digitraffic/tie/service/v2/maintenance/V2MaintenanceRealizationDataService.java
+++ src/main/java/fi/livi/digitraffic/tie/service/v2/maintenance/V2MaintenanceRealizationDataService.java
@@ -5,7 +5,6 @@ import static fi.livi.digitraffic.tie.helper.DateHelper.toZonedDateTimeAtUtc;
 import java.time.Instant;
 import java.time.ZonedDateTime;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
