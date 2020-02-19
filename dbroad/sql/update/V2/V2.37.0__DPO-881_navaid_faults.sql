f66ee62809ca3812fa166aa222c1990a30e28b4d
Teijo Rönkä <teijo.ronka@solita.fi>
2020-02-19 12:06:22 +0200

    DPO-881 first version of db

---
+++ dbroad/sql/update/V2/V2.37.0__DPO-881_navaid_faults.sql
@@ -0,0 +1,21 @@
+create table navaid_fault (
+	id bigint primary key,
+	entry_timestamp timestamp(0) with time zone not null,
+	fixed_timestamp timestamp(0) with time zone,
+	domain text not null,
+	state text not null,
+	type text not null, 
+	fixed boolean not null,
+	navaid_id int not null,
+	navaid_name_fi text not null,
+	navaid_name_se text not null,
+	navaid_type_fi text not null,
+	navaid_type_se text not null,
+	fairway_number int not null,
+	fairway_name_fi text not null,
+	fairway_name_se text not null,
+	area_number int not null,
+	area_description_fi text not null,
+	area_description_se text not null,
+	geometry geometry not null
+);
