40f71ecdde94cae4c28bb20b884ca8c91b308324
Jouni Sonninen <jouni.sonninen@solita.fi>
2020-07-31 11:19:10 +0200

    DPO-1108 HARJA työkoneen seurantatiedon käsittelyn tehostaminen - update query performance

--- dbroad/sql/update/V2/V2.50.0__DPO-1108_HARJA_performance.sql
+++ dbroad/sql/update/V2/V2.50.0__DPO-1108_HARJA_performance.sql
@@ -1,3 +1,4 @@
--- Execution time dropped to 1/4 of the original
+-- Execution time of finding last tracking of work machine dropped to 1/4 of the original
+-- and this is asked quite a lot
 CREATE INDEX maintenance_tracking_not_finished_work_machine_id_i
 ON maintenance_tracking USING BTREE (work_machine_id) WHERE finished = false;
