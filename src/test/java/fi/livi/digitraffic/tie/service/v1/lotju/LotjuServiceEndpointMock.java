7864c62380ab5d99a1f5754afbe5ebe307468568
Jouni Sonninen <jouni.sonninen@solita.fi>
2020-08-18 14:08:12 +0200

    DPO-1127 TVT-integraatio: Tuki useammalla http-nodelle - Camera image support for multiple nodes

--- src/test/java/fi/livi/digitraffic/tie/service/v1/lotju/LotjuServiceEndpointMock.java
+++ src/test/java/fi/livi/digitraffic/tie/service/v1/lotju/LotjuServiceEndpointMock.java
@@ -63,7 +63,7 @@ public abstract class LotjuServiceEndpointMock {
     }
 
     public boolean isNotInited() {
-        return inited;
+        return !inited;
     }
 
     public boolean isStateAfterChange() {
