c6f06e324128510270c9435e399a9754d0f9b296
Massimo Prencipe <massimo.prencipe@solita.fi>
2020-10-05 09:05:17 +0300

    DPO-1201 add constraints, increase id column size

--- dbroad/sql/update/V2/V2.54.0__DPO-1201_open311_subjects.sql
+++ dbroad/sql/update/V2/V2.54.0__DPO-1201_open311_subjects.sql
@@ -1,19 +1,25 @@
 CREATE TABLE open311_subject (
-     id SMALLINT NOT NULL,
+     id INTEGER NOT NULL,
      active SMALLINT NOT NULL,
      name TEXT NOT NULL,
      locale CHARACTER VARYING (2) NOT NULL,
      PRIMARY KEY (id, locale)
 );
 
+ALTER TABLE open311_subject ADD CONSTRAINT open311_subject_locale_check CHECK (locale IN ('fi', 'sv', 'en'));
+ALTER TABLE open311_subject ADD CONSTRAINT open311_subject_active_check CHECK (active IN (0, 1));
+
 CREATE TABLE open311_subsubject (
-     id SMALLINT NOT NULL,
+     id INTEGER NOT NULL,
      subject_id SMALLINT NOT NULL,
      active SMALLINT NOT NULL,
      name TEXT NOT NULL,
      locale CHARACTER VARYING (2) NOT NULL,
      PRIMARY KEY (id, locale)
 );
+
+ALTER TABLE open311_subsubject ADD CONSTRAINT open311_subsubject_locale_check CHECK (locale IN ('fi', 'sv', 'en'));
+ALTER TABLE open311_subsubject ADD CONSTRAINT open311_subsubject_active_check CHECK (active IN (0, 1));
 
 -- keys were numbers after all
 ALTER TABLE open311_service_request_state ALTER COLUMN key TYPE SMALLINT USING (key::SMALLINT);
