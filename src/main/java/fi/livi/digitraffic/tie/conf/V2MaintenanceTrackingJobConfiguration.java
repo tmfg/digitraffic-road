b9206c406f9a3ebbcab5fb5daa69581186292290
Jouni Sonninen <jouni.sonninen@solita.fi>
2020-11-02 12:05:58 +0100

    DPO-1225 HARJA vastauksen muodostaminen kuormittaa sovellusta kovasti - Restored fixedRateString -> fixedDelayString

--- src/main/java/fi/livi/digitraffic/tie/conf/V2MaintenanceTrackingJobConfiguration.java
+++ src/main/java/fi/livi/digitraffic/tie/conf/V2MaintenanceTrackingJobConfiguration.java
@@ -42,7 +42,7 @@ public class V2MaintenanceTrackingJobConfiguration {
      * This job extracts all unhandled maintenance trackings
      * from source JSON-format to db relations.
      */
-    @Scheduled(fixedRateString = "${maintenance.tracking.job.intervalMs}")
+    @Scheduled(fixedDelayString = "${maintenance.tracking.job.intervalMs}")
     public void handleUnhandledMaintenanceTrackings() {
         final StopWatch start = StopWatch.createStarted();
         int count;
