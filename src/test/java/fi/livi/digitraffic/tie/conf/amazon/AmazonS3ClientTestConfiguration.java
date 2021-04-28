d6a2c52a1f6325b6216ce55f2770e8f4af6ffc9c
Teijo Rönkä <teijo.ronka@solita.fi>
2021-04-28 14:57:38 +0300

    DPO-1330 new try

--- src/test/java/fi/livi/digitraffic/tie/conf/amazon/AmazonS3ClientTestConfiguration.java
+++ src/test/java/fi/livi/digitraffic/tie/conf/amazon/AmazonS3ClientTestConfiguration.java
@@ -11,31 +11,28 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.utility.DockerImageName;
 
-//@ConditionalOnNotWebApplication
-//@Configuration
+import static org.testcontainers.containers.localstack.LocalStackContainer.Service.S3;
+import static org.testcontainers.containers.localstack.LocalStackContainer.Service.SQS;
+
+@ConditionalOnNotWebApplication
+@Configuration
 public class AmazonS3ClientTestConfiguration {
 
-/*    @Bean
-    public LocalstackDocker localstackDocker() {
-        return LocalstackDocker.getLocalstackDocker();
-    }
+    @Container
+    static LocalStackContainer localStack =
+        new LocalStackContainer(DockerImageName.parse("localstack/localstack:0.10.0"))
+            .withServices(S3, SQS)
+            .withEnv("DEFAULT_REGION", "eu-central-1");
 
     @Bean
-    public AmazonDockerClientsHolder amazonDockerClientsHolder(final LocalstackDocker localstackDocker) {
-        return new AmazonDockerClientsHolder(localstackDocker);
-    }
-*/
-    @ConditionalOnExpression("'${spring.localstack.enabled}' == 'false'")
-    @Bean
-    public AmazonS3 amazonS3(final @Value("${dt.amazon.s3.weathercam.region}") String region) {
-
-        final AWSCredentials credentials = new BasicAWSCredentials( "dummy",  "dummy");
-        final AmazonS3ClientBuilder builder =
-            AmazonS3ClientBuilder
-                .standard()
-                .withCredentials(new AWSStaticCredentialsProvider(credentials))
-                .withRegion(region);
-        return builder.build();
+    public AmazonS3 amazonS3() {
+        return AmazonS3ClientBuilder.standard()
+            .withCredentials(localStack.getDefaultCredentialsProvider())
+            .withEndpointConfiguration(localStack.getEndpointConfiguration(S3))
+            .build();
     }
 }
