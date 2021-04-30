2c98f098bd58a7e87ddb3969bdeb87718e976252
Teijo Rönkä <teijo.ronka@solita.fi>
2021-04-30 07:40:17 +0300

    DPO-1330 remove @RunWith

--- src/test/java/fi/livi/digitraffic/tie/service/v1/location/MetadataFileFetcherIntegrationTest.java
+++ src/test/java/fi/livi/digitraffic/tie/service/v1/location/MetadataFileFetcherIntegrationTest.java
@@ -1,20 +1,16 @@
 package fi.livi.digitraffic.tie.service.v1.location;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-import fi.livi.digitraffic.tie.service.ObjectNotFoundException;
-import org.junit.jupiter.api.Test;import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.io.FileNotFoundException;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@RunWith(SpringJUnit4ClassRunner.class)
 @Import(MetadataFileFetcher.class)
 public class MetadataFileFetcherIntegrationTest {
     @Test
-    public void testLocationNotFound() throws IOException {
+    public void testLocationNotFound() {
         final MetadataFileFetcher fetcher = new MetadataFileFetcher("https://aineistot.vayla.fi/not_found");
 
         assertThrows(FileNotFoundException.class, () -> {
