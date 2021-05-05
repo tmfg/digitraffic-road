6af7a946e3864b12f446c14b25b62d222ee191fd
Jouni Sonninen <jouni.sonninen@solita.fi>
2021-05-05 13:51:23 +0300

    DPO-1321 Simppeli Json versioiden puuttuvista kentistä varoituksien siivoaminen

--- src/main/java/fi/livi/digitraffic/tie/model/JsonAdditionalProperties.java
+++ src/main/java/fi/livi/digitraffic/tie/model/JsonAdditionalProperties.java
@@ -1,12 +1,14 @@
 package fi.livi.digitraffic.tie.model;
 
+import static com.google.common.collect.Sets.newHashSet;
 import static org.slf4j.LoggerFactory.getLogger;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.fasterxml.jackson.annotation.JsonAnySetter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import fi.livi.digitraffic.tie.helper.LoggerHelper;
 
@@ -19,14 +21,38 @@ import fi.livi.digitraffic.tie.helper.LoggerHelper;
  */
 public abstract class JsonAdditionalProperties {
 
-    @JsonIgnore
-    private final Map<String, Object> additionalProperties = new HashMap<>();
+    private final Map<Class<?>, Set<String>> ignoreClassToPropertiesMap = Stream.of(new Object[][] {
+        { fi.livi.digitraffic.tie.dto.v3.trafficannouncement.geojson.RoadWorkPhase.class,
+          newHashSet("features") },
+        { fi.livi.digitraffic.tie.dto.v2.trafficannouncement.geojson.TrafficAnnouncementProperties.class,
+          newHashSet("situationType", "trafficAnnouncementType", "locationToDisplay") },
+        { fi.livi.digitraffic.tie.dto.v3.trafficannouncement.geojson.TrafficAnnouncementProperties.class,
+          newHashSet("locationToDisplay") },
+        { fi.livi.digitraffic.tie.dto.v2.trafficannouncement.geojson.TrafficAnnouncement.class,
+          newHashSet("lastActiveItinerarySegment", "roadWorkPhases", "earlyClosing") },
+        { fi.livi.digitraffic.tie.dto.v3.trafficannouncement.geojson.Contact.class,
+          newHashSet("fax") },
+        { fi.livi.digitraffic.tie.dto.v3.trafficannouncement.geojson.TrafficAnnouncementFeature.class,
+          newHashSet("bbox") },
 
+
+    }).collect(Collectors.toMap(data -> (Class<?>)data[0], data -> (Set<String>)data[1]));
+
+    /**
+     * This is called by jackson when converting serialized json to objects
+     */
     @JsonAnySetter
     public void setAdditionalProperty(final String property, final Object value) {
-        this.additionalProperties.put(property, value);
-        // Log missing properties to notify
-        getLogger(getClass()).warn("method=setAdditionalProperty No property found with the name: {} and value: {}",
-                                   property, LoggerHelper.objectToStringLoggerSafe(value));
+        // Don's save the value, just log unmapped values
+        if (isLoggable(property)) {
+            // Log missing properties
+            getLogger(getClass()).warn("method=setAdditionalProperty No property found with the name: {} and value: {}",
+                property, LoggerHelper.objectToStringLoggerSafe(value));
+        }
+    }
+
+    private boolean isLoggable(final String property) {
+        final Set<String> set = ignoreClassToPropertiesMap.get(getClass());
+        return set == null || !set.contains(property);
     }
 }
