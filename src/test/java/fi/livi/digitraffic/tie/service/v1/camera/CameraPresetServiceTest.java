26ff6f88598630d05461aa1da898b4074ec27d3e
Jouni Sonninen <jouni.sonninen@solita.fi>
2021-05-24 12:58:45 +0300

    DPO-1399 Kameroiden metadatan päivitys ei onnistu - fixed find by lotju id

--- src/test/java/fi/livi/digitraffic/tie/service/v1/camera/CameraPresetServiceTest.java
+++ src/test/java/fi/livi/digitraffic/tie/service/v1/camera/CameraPresetServiceTest.java
@@ -1,5 +1,8 @@
 package fi.livi.digitraffic.tie.service.v1.camera;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.Map;
 
 import org.junit.jupiter.api.Test;
@@ -8,16 +11,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import fi.livi.digitraffic.tie.AbstractServiceTest;
 import fi.livi.digitraffic.tie.model.v1.camera.CameraPreset;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 public class CameraPresetServiceTest extends AbstractServiceTest {
 
     @Autowired
     private CameraPresetService cameraPresetService;
 
     @Test
-    public void testFindAll() {
+    public void findAll() {
         final Map<Long, CameraPreset> all = cameraPresetService.findAllCameraPresetsMappedByLotjuId();
         assertTrue(all.size() > 0);
     }
+
+    @Test
+    public void findCameraPresetByLotjuId() {
+        // Create 3 presets with same lotju_id
+        final CameraPreset ps1 = generateDummyPreset();
+        final CameraPreset ps2 = generateDummyPreset();
+        final CameraPreset ps3 = generateDummyPreset();
+        ps2.setLotjuId(ps1.getLotjuId());
+        ps3.setLotjuId(ps1.getLotjuId());
+        ps1.obsolete();
+        ps3.obsolete();
+        cameraPresetService.save(ps1);
+        cameraPresetService.save(ps2);
+        cameraPresetService.save(ps3);
+
+        // Should find the one with null obsolete date
+        final CameraPreset cp = cameraPresetService.findCameraPresetByLotjuId(ps1.getLotjuId());
+        assertEquals(ps2.getId(), cp.getId());
+    }
+
 }
