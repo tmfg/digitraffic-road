cf1342a819025e02051a2526d9c90c1c716a2549
Teijo Rönkä <teijo.ronka@solita.fi>
2021-06-01 20:27:15 +0300

    DPO-1392 try to inject port

--- src/test/java/fi/livi/digitraffic/tie/service/v1/lotju/LotjuMockConfiguration.java
+++ src/test/java/fi/livi/digitraffic/tie/service/v1/lotju/LotjuMockConfiguration.java
@@ -29,8 +29,9 @@ public class LotjuMockConfiguration {
             @Qualifier("kameraMetadataJaxb2Marshaller")
             final Jaxb2Marshaller kameraMetadataJaxb2Marshaller,
             @Value("${metadata.server.addresses}") final String serverAddresses,
+            @Value("#{T(fi.livi.digitraffic.tie.AbstractTest).LOTJU_SERVICE_RANDOM_PORT}") final String port,
             @Value("${metadata.server.path.camera}") final String dataPath) {
-        final String srv = StringUtils.split(serverAddresses, ",")[0];
+        final String srv = StringUtils.split(serverAddresses, ",")[0] + ":" + port;
         return LotjuKameraPerustiedotServiceEndpointMock.getInstance(srv + dataPath, resourceLoader, kameraMetadataJaxb2Marshaller);
     }
 
@@ -40,8 +41,9 @@ public class LotjuMockConfiguration {
             @Qualifier("lamMetadataJaxb2Marshaller")
             final Jaxb2Marshaller lamMetadataJaxb2Marshaller,
             @Value("${metadata.server.addresses}") final String serverAddresses,
+            @Value("#{T(fi.livi.digitraffic.tie.AbstractTest).LOTJU_SERVICE_RANDOM_PORT}") final String port,
             @Value("${metadata.server.path.tms}") final String dataPath) {
-        final String srv = StringUtils.split(serverAddresses, ",")[0];
+        final String srv = StringUtils.split(serverAddresses, ",")[0] + ":" + port;
         return LotjuLAMMetatiedotServiceEndpointMock.getInstance(srv + dataPath, resourceLoader, lamMetadataJaxb2Marshaller);
     }
 
@@ -51,8 +53,9 @@ public class LotjuMockConfiguration {
             @Qualifier("tiesaaMetadataJaxb2Marshaller")
             final Jaxb2Marshaller tiesaaMetadataJaxb2Marshaller,
             @Value("${metadata.server.addresses}") final String serverAddresses,
+            @Value("#{T(fi.livi.digitraffic.tie.AbstractTest).LOTJU_SERVICE_RANDOM_PORT}") final String port,
             @Value("${metadata.server.path.weather}") final String dataPath) {
-        final String srv = StringUtils.split(serverAddresses, ",")[0];
+        final String srv = StringUtils.split(serverAddresses, ",")[0] + ":" + port;
         return LotjuTiesaaPerustiedotServiceEndpointMock.getInstance(srv + dataPath, resourceLoader, tiesaaMetadataJaxb2Marshaller);
     }
 
