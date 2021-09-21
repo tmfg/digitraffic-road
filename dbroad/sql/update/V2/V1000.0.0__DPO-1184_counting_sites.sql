45b64e0e27688bee071af37dc1b5c39822c0bfa4
Teijo Rönkä <teijo.ronka@solita.fi>
2021-09-21 12:18:41 +0300

    DPO-1184 review fixes

--- dbroad/sql/update/V2/V1000.0.0__DPO-1184_counting_sites.sql
+++ dbroad/sql/update/V2/V1000.0.0__DPO-1184_counting_sites.sql
@@ -1,17 +1,17 @@
-create table cs_domain (
+create table counting_site_domain (
 	name 			text 	primary key,
 	description 	text,
-	added_date		timestamp with time zone,
-	removed_date	timestamp with time zone null
+	added_date		timestamp(0) with time zone,
+	removed_date	timestamp(0) with time zone null
 );
 
-create table cs_user_type (
+create table counting_site_user_type (
 	id				smallint	primary key,
 	name			text
 );
 
-create table counting_site (
-	id				bigint 	primary key,
+create table counting_site_counter (
+	id				bigint 	    primary key,
 	site_id			integer,
 	domain_name		text,
 	site_domain		text,
@@ -19,24 +19,24 @@ create table counting_site (
 	user_type		smallint,
 	interval		smallint,
 	direction		smallint,
-	added_date		timestamp with time zone,
-	removed_date	timestamp with time zone null
+	added_date		timestamp(0) with time zone,
+	removed_date	timestamp(0) with time zone null
 );
 
-alter table counting_site add constraint counting_site_domain_fkey foreign key (domain_name) references cs_domain(name);
-alter table counting_site add constraint counting_site_user_type_fkey foreign key (user_type) references cs_user_type(id);
+alter table counting_site_counter add constraint counting_site_counter_domain_fkey foreign key (domain_name) references counting_site_domain(name);
+alter table counting_site_counter add constraint counting_site_counter_user_type_fkey foreign key (user_type) references counting_site_user_type(id);
 
-create index counting_site_domain_fk on counting_site(domain_name);
-create index counting_site_user_type_fk on counting_site(user_type);
+create index counting_site_counter_domain_fk on counting_site_counter(domain_name);
+create index counting_site_counter_user_type_fk on counting_site_counter(user_type);
 
-create table cs_data (
+create table counting_site_data (
 	id					bigint	primary key,
-	counting_site_id	bigint,
-	data_timestamp		timestamp with time zone,
+	counter_id			bigint,
+	data_timestamp		timestamp(0) with time zone,
 	count				integer null,
 	status				smallint null
 );
 
-alter table cs_data add constraint cs_data_cs_fkey foreign key (counting_site_id) references counting_site(id);
+alter table counting_site_data add constraint counting_site_data_counter_fkey foreign key (counter_id) references counting_site_counter(id);
 
-create index cs_data_cs_fk on cs_data(counting_site_id);
+create index counting_site_data_site_fko on counting_site_counter(site_id);
