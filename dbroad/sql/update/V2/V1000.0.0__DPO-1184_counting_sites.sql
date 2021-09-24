36798cdf7ac2dcd7a5f4d50af22f5043d72c0efd
Teijo Rönkä <teijo.ronka@solita.fi>
2021-09-24 13:18:55 +0300

    DPO-1184 fix fk-index

--- dbroad/sql/update/V2/V1000.0.0__DPO-1184_counting_sites.sql
+++ dbroad/sql/update/V2/V1000.0.0__DPO-1184_counting_sites.sql
@@ -46,7 +46,7 @@ alter table counting_site_data add constraint counting_site_data_counter_fkey fo
 
 create sequence counting_site_data_id_seq;
 
-create index counting_site_data_site_fki on counting_site_counter(site_id);
+create index counting_site_data_counter_fki on counting_site_data(counter_id);
 
 insert into counting_site_user_type(id, name)
     values
