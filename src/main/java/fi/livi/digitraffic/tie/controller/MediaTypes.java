ed1928acd4d0878957374170e0836e33950ccc36
Jouni Sonninen <jouni.sonninen@solita.fi>
2021-11-04 08:02:37 +0100

    DPO-1603 Mobiilisti REST-apien ääkköset eivät toimi

--- src/main/java/fi/livi/digitraffic/tie/controller/MediaTypes.java
+++ src/main/java/fi/livi/digitraffic/tie/controller/MediaTypes.java
@@ -4,9 +4,10 @@ import org.springframework.http.MediaType;
 
 public class MediaTypes {
 
-    public static final String MEDIA_TYPE_APPLICATION_VND_GEO_JSON = "application/vnd.geo+json";
-    public static final String MEDIA_TYPE_APPLICATION_GEO_JSON = "application/geo+json";
-    public static final String MEDIA_TYPE_APPLICATION_JSON = MediaType.APPLICATION_JSON_VALUE;
+    /* Set also charset=UTF-8 as mobile applications wont always work without it. */
+    public static final String MEDIA_TYPE_APPLICATION_VND_GEO_JSON = "application/vnd.geo+json;charset=UTF-8";
+    public static final String MEDIA_TYPE_APPLICATION_GEO_JSON = "application/geo+json;charset=UTF-8";
+    public static final String MEDIA_TYPE_APPLICATION_JSON = MediaType.APPLICATION_JSON_UTF8_VALUE;
 
     private MediaTypes() {}
 }
