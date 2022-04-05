e197649d62ad5bc8a03f64397916fcb1661ecd27
Jouni Sonninen <jouni.sonninen@solita.fi>
2022-04-05 13:39:53 +0200

    DPO-1724 Kunnossapitodatan rajapintaan tuki muutosaikaleimalle - review fix

--- dbroad/sql/update/V2/V99.89.0__DPO-1724_maintenance_tracking_created_time_search.sql
+++ dbroad/sql/update/V2/V99.89.0__DPO-1724_maintenance_tracking_created_time_search.sql
@@ -1,2 +1,2 @@
 CREATE INDEX IF NOT EXISTS maintenance_tracking_end_created_domain_i ON maintenance_tracking USING btree (end_time, created, domain);
-CREATE INDEX maintenance_tracking_created_i ON maintenance_tracking (created, domain);
+CREATE INDEX IF NOT EXISTS maintenance_tracking_created_i ON maintenance_tracking (created, domain);
