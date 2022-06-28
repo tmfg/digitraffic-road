51216d601241bcabd9acae5cdd2b4b123a882ca0
Teijo Rönkä <teijo.ronka@solita.fi>
2022-06-28 11:47:42 +0300

    handle invalid datex2 messagetypes

--- src/main/java/fi/livi/digitraffic/tie/converter/Datex2MessagetypeParameterStringToEnumConverter.java
+++ src/main/java/fi/livi/digitraffic/tie/converter/Datex2MessagetypeParameterStringToEnumConverter.java
@@ -4,10 +4,17 @@ import org.springframework.core.convert.converter.Converter;
 
 import fi.livi.digitraffic.tie.model.v1.datex2.Datex2MessageType;
 
+@Deprecated
 public class Datex2MessagetypeParameterStringToEnumConverter implements Converter<String, Datex2MessageType> {
 
     @Override
     public Datex2MessageType convert(final String from) {
-        return Datex2MessageType.valueOf(from.replace("-", "_").toUpperCase());
+        final Datex2MessageType type = Datex2MessageType.valueOf(from.replace("-", "_").toUpperCase());
+
+        if (type == null) {
+            throw new IllegalArgumentException("Invalid messagetype " + from);
+        }
+
+        return type;
     }
 }
