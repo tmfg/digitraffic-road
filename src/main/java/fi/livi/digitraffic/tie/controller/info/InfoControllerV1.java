88b650dc6af51700649dd59120b8eeb8e411ec36
Ilari Junnonen <92532596+solita-ijunnone@users.noreply.github.com>
2022-12-07 14:23:33 +0200

    Remove description from InfoController

--- src/main/java/fi/livi/digitraffic/tie/controller/info/InfoControllerV1.java
+++ src/main/java/fi/livi/digitraffic/tie/controller/info/InfoControllerV1.java
@@ -19,7 +19,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
-@Tag(name = INFO_TAG_V1, description = "Weather Controller")
+@Tag(name = INFO_TAG_V1)
 @RestController
 @Validated
 @ConditionalOnWebApplication
