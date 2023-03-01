25c51be9e842bd006affe63b5cf7c6ac31d0c67b
Joonas-M-S <joonas.mikkonen@solita.fi>
2023-03-01 14:39:38 +0200

    DPO-1977: Injection test

--- src/test/java/fi/livi/digitraffic/tie/IkavaTest.java
+++ src/test/java/fi/livi/digitraffic/tie/IkavaTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 public class IkavaTest {
     @Test
     public void ikavaTesti() {
+        System.out.println("Runner OS: ${{ runner.os }}");
         Map<String, String> env = System.getenv();
         for (String envName : env.keySet()) {
             System.out.println(envName);
