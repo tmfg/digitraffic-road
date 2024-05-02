fa5aa2c80449617f7419dcbc4304257692c5a3e5
Ilari Junnonen <ilari.junnonen@solita.fi>
2024-05-02 13:42:13 +0300

    DPO-2402 Move functions and triggers

--- dbroad/sql/update/V3/V3.13.0__DPO-2402__camera_preset_pic_last_modified_db.sql
+++ dbroad/sql/update/V3/V3.13.0__DPO-2402__camera_preset_pic_last_modified_db.sql
@@ -2,16 +2,6 @@
 
 -- column 'modified' in camera_preset must not be updated when 'pic_last_modified' is updated,
 -- so this value is used instead for tracking preset update times
-ALTER TABLE camera_preset ADD COLUMN pic_last_modified_db TIMESTAMP(0) WITH TIME ZONE;
+ALTER TABLE camera_preset ADD COLUMN IF NOT EXISTS pic_last_modified_db TIMESTAMP(0) WITH TIME ZONE;
 
-CREATE OR REPLACE FUNCTION update_pic_last_modified_db_column()
-  RETURNS TRIGGER AS $$
-BEGIN
-  IF (to_jsonb(OLD.pic_last_modified) <> to_jsonb(NEW.pic_last_modified)) THEN
-    NEW.pic_last_modified_db = now();
-END IF;
-RETURN NEW;
-END;
-$$ language 'plpgsql';
 
-CREATE TRIGGER camera_preset_pic_last_modified_db_t BEFORE UPDATE ON camera_preset FOR EACH ROW EXECUTE PROCEDURE update_pic_last_modified_db_column();
